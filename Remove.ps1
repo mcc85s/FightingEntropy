159121242db101a10777ebe9e35cf3c0d322a63c
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-06-01 15:12:39 -0400

    Update Remove.ps1

--- Remove.ps1
+++ Remove.ps1
@@ -2,7 +2,7 @@ $RegPath  = 'HKLM:\software\Policies\Secure Digits Plus LLC\FightingEntropy'
 $Version  = Get-ChildItem $RegPath | % PSChildName | Select-Object -Last 1
 $Trunk    = Get-ItemProperty "$RegPath\$Version"
 $DataPath = $Trunk.Path
-$ModPath  = $Env:PSModulePath -Split ";" | ? { ( Test-Path $_ ) -and ( Get-ChildItem $_ | ? Name -match FightingEntropy ) } | % FullName
+$ModPath  = "$($Env:PSModulePath -Split ";" | ? { (Test-Path $_) -and (Get-ChildItem $_ | ? Name -match FightingEntropy)})\FightingEntropy"
 
 "Classes","Control","Functions","Graphics" | % {
 
