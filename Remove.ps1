655317adb6f4aae39959a82c0aea4fd80649cb8f
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-06-01 19:49:49 -0400

    Update Remove.ps1

--- Remove.ps1
+++ Remove.ps1
@@ -1,17 +1,22 @@
 $Name     = "FightingEntropy"
 $Version  = "2021.6.0"
 $Company  = "Secure Digits Plus LLC"
-$Default  = $Env:PSModulePath -Split ";" | ? { (Test-Path $_) -and (Get-ChildItem $_ | ? Name -match FightingEntropy })
+
+# [Default Path]
+$Default  = $Env:PSModulePath -Split ";" | ? { (Test-Path $_) -and (Get-ChildItem $_ | ? Name -match FightingEntropy) }
 If (!$Default)
 {
     Write-Host "[!] Missing module"
 }
+
+# [Registry Path]
 $RegPath  = "HKLM:\Software\Policies\$Company\$Name\$Version"
 If (!(Test-Path $RegPath))
 {
     Write-Host "[!] Missing registry"
 }
 
+# [Property Value]
 $RegValue = Get-ItemProperty $RegPath -EA 0
 $ModPath  = "$Default\FightingEntropy"
 $DataPath = "$Env:ProgramData\$Company\$Name"
