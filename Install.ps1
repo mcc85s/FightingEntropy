456f636c43108591ea3d52992ff28201a5e8676f
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-05-31 22:42:00 -0400

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -40,66 +40,188 @@ Function FightingEntropy
         }
     }
 
-    Class _Install
+    Class _OS
     {
-        [String]           $Root = "$Env:ProgramData\Secure Digits Plus LLC\FightingEntropy"
-        [String]           $Base = "github.com/mcc85s/FightingEntropy/blob/main"
-        [String[]]        $Names = ("Classes Control Functions Graphics" -Split " ")
-        [String]           $GUID = "ccd91f81-eec0-4a77-9fe2-0447245a9f54"
-        [String]        $Version = "2021.6.0"
-        [String]        $Default
-        [String]           $Main
-        [String]          $Trunk
-        [String]        $ModPath
-        [String]        $ManPath
-        [String]        $RegPath
-        [String]       $DataPath
+        [Object] $Env
+        [Object] $Var
+        [Object] $PS
+        [Object] $Ver
+        [Object] $Major
+        [Object] $Type
 
-        Hidden [Hashtable] $Hash = @{
-
-            Classes   = ("Manifest Hive Root Install Module OS Info RestObject Host FirewallRule Drive Drives ViperBomb File Cache Icons",
-            "Shortcut Brand Branding DNSSuffix DomainName ADLogin ADConnection FEDCPromo Certificate Company Key RootVar Share Source",
-            "Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS Image Images Updates Role Win32_Client Win32_Server",
-            "UnixBSD RHELCentOS DCFound" -join ' ') -Split " " | % { "_$_.ps1" }
-            Control   = "Computer.png DefaultApps.xml $( "FE","MDT","PSD" | % { "$_`Client","$_`Server" } | % { "$_`Mod.xml" } )" -Split " "
-            Functions = ("Add-ACL","Complete-IISServer","Export-Ini","Get-Certificate","Get-DiskInfo","Get-FEDCPromo","Get-FEDCPromoProfile",
-            "Get-FEHive","Get-FEHost","Get-FEImage","Get-FEManifest","Get-FEModule","Get-FENetwork","Get-FEOS","Get-FEService","Get-FEShare",
-            "Get-MadBomb","Get-MDTModule","Get-ServerDependency","Get-ViperBomb","Get-XamlWindow","Import-FEImage","Install-FEModule",
-            "Install-IISServer","New-ACLObject","New-Company","New-EnvironmentKey","New-FEImage","New-FEShare","Remove-FEModule","Remove-FEShare",
-            "Show-ToastNotification","Update-FEShare","Write-Theme","Get-MDTOData","New-FEDeploymentShare","Start-VMGroup",
-            "Install-VMGroup" | % { "$_.ps1" })
-            Graphics  = "background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp sdplogo.png" -Split " "
+        _OS()
+        {
+            $This.Env   = Get-ChildItem Env:\
+            $This.Var   = Get-ChildItem Variable:\
+            $This.PS    = Get-Item Variable:\PSVersionTable | % Value
+            $This.Ver   = Get-Item Variable:\PSVersionTable | % Value | % PSVersion
+            $This.Major = $This.Ver.Major
+            $This.Type  = $This.GetOSType()
         }
 
+        [Object] GetItem([String]$Item)
+        {
+            $Return = @{ }
+
+            ForEach ( $X in ( Get-Item -Path $Item | % GetEnumerator ) )
+            { 
+                $Return.Add($X.Name,$X.Value)
+            }
+
+            Return $Return
+        }
+
+        [String] GetWinType()
+        {
+            Return @( Switch -Regex ( Invoke-Expression "[wmiclass]'Win32_OperatingSystem' | % GetInstances | % Caption" )
+            {
+                "Windows 10" { "Win32_Client" } "Windows Server" { "Win32_Server" }
+            })
+        }
+
+        [String] GetOSType()
+        {
+            Return @( If ( $This.Major -gt 5 )
+            {
+                If ( Get-Item Variable:\IsLinux | % Value )
+                {
+                    "RHELCentOS"
+                }
+
+                Else
+                {
+                    $This.GetWinType()
+                }
+            }
+
+            Else
+            {
+                $This.GetWinType()
+            })
+        }
+    }
+
+    Class _Manifest
+    {
+        [String[]]    $Classes = @(("Manifest Hive Root Install Module OS Info RestObject Host FirewallRule Drive Drives ViperBomb File Cache Icons",
+                "Shortcut Brand Branding DNSSuffix DomainName ADLogin ADConnection FEDCPromo Certificate Company Key RootVar Share Source",
+                "Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS Image Images Updates Role Win32_Client Win32_Server",
+                "UnixBSD RHELCentOS DCFound" -join ' ') -Split " " | % { "_$_.ps1" })
+
+        [String[]]    $Control = "Computer.png DefaultApps.xml $( "FE","MDT","PSD" | % { "$_`Client","$_`Server" } | % { "$_`Mod.xml" } )" -Split " "
+
+        [String[]]  $Functions = ("Add-ACL","Complete-IISServer","Export-Ini","Get-Certificate","Get-DiskInfo","Get-FEDCPromo","Get-FEDCPromoProfile",
+                "Get-FEHive","Get-FEHost","Get-FEImage","Get-FEManifest","Get-FEModule","Get-FENetwork","Get-FEOS","Get-FEService","Get-FEShare",
+                "Get-MadBomb","Get-MDTModule","Get-ServerDependency","Get-ViperBomb","Get-XamlWindow","Import-FEImage","Install-FEModule",
+                "Install-IISServer","New-ACLObject","New-Company","New-EnvironmentKey","New-FEImage","New-FEShare","Remove-FEModule","Remove-FEShare",
+                "Show-ToastNotification","Update-FEShare","Write-Theme","Get-MDTOData","New-FEDeploymentShare","Start-VMGroup",
+                "Install-VMGroup" | % { "$_.ps1" })
+
+        [String[]]   $Graphics = "background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp sdplogo.png" -Split " "
+
+        _Manifest()
+        {
+
+        }
+    }
+
+    Class _Registry
+    {
+        [String] $Name
+        [String] $Date
+        [String] $Path
+        [String] $Provider
+        [String] $Status
+        [String] $Type
+        [String] $Version
+
+        _Registry([String]$Company,[String]$Name,[String]$Version,[String]$Type)
+        {
+            $This.Name        = $Name
+            $This.Date        = Get-Date -UFormat "%Y_%m%d-%H%M%S"
+            $This.Provider    = $Company
+            $This.Status      = "Initialized"
+            $This.Type        = $Type
+            $This.Version     = $Version
+
+            ForEach ( $Item in $Company, $Name, $Version )
+            {
+                If ( $This.Path -eq $Null )
+                {
+                    $This.Path = "HKLM:\Software\Policies\$Item"
+                }
+
+                Else
+                {
+                    $This.Path = $This.Path, $Item -join "\"
+                }
+
+                If (!(Test-Path $This.Path))
+                {
+                    New-Item -Path (Split-Path $This.Path) -Name $Item -Verbose
+                }
+            }
+
+            ForEach ( $Key in "Date Name Path Provider Status Type Version".Split(" ") )
+            {
+                If ((Get-ItemProperty $This.Path ).$Key -ne $This.$Key )
+                {
+                    Set-ItemProperty $This.Path -Name $Key -Value $This.$Key -Verbose
+                }
+            }
+        }
+    }
+
+    Class _Module
+    {
+        [String]        $Base = "github.com/mcc85s/FightingEntropy/blob/main"
+        [String]        $Name = "FightingEntropy"
+        [String]      $Author = "Michael C. Cook Sr."
+        [String]     $Company = "Secure Digits Plus LLC"
+        [String]        $GUID = "ccd91f81-eec0-4a77-9fe2-0447245a9f54"
+        [String]     $Version 
+        [Object]          $OS = [_OS]::New()
+        [Object]    $Manifest = [_Manifest]::New()
+        [Object]    $Registry
+        [String]     $Default
+        [String]        $Main
+        [String]       $Trunk
+        [String]     $ModPath
+        [String]     $ManPath
+        [String]        $Path
+        [Object]        $Tree
         [Object[]]   $Classes
         [Object[]]   $Control
         [Object[]] $Functions
         [Object[]]  $Graphics
 
-        [String[]] List([String]$Type)
+        _Build()
         {
-            Return @( IRM "$($This.Base)/$Type/index.txt?raw=true" ) -Split "`n" | ? Length -gt 0
-        }
+            $_Path = $Null
 
-        Build()
-        {
-            $Path = $Null
-
-            ForEach ( $X in $This.Root.Split("\"))
+            ForEach ( $Item in $This.Path.Split("\") )
             {
-                If ( $Path -eq $Null )
+                If ( $_Path -eq $Null )
                 {
-                    $Path = $X
+                    $_Path = $Item
                 }
 
                 Else
                 {
-                    $Path = "$Path\$X"
+                    $_Path = "$_Path\$Item"
                 }
 
-                If (!(Test-Path $Path))
+                If (!(Test-Path $_Path))
                 {
-                    New-Item $Path -ItemType Directory -Verbose
+                    New-Item $_Path -ItemType Directory -Verbose
+                }
+            }
+
+            "Classes","Control","Functions","Graphics" | % { 
+
+                If (!(Test-Path "$_Path\$_"))
+                {
+                    New-Item "$_Path\$_" -ItemType Directory -Verbose
                 }
             }
         }
@@ -107,9 +229,12 @@ Function FightingEntropy
         Gather()
         {
             $O = 1
-            ForEach ( $Object in $This.Classes, $This.Control, $This.Functions, $This.Graphics )
+            ForEach ( $Object in $This.Classes, 
+                                 $This.Control, 
+                                 $This.Functions, 
+                                 $This.Graphics )
             {
-                $Type = $This.Names[$O-1]
+                $Type = "Classes Control Functions Graphics".Split(" ")[$O-1]
                 Write-Host "Gathering $Type [$O/4]"
                 $I = 1
 
@@ -127,9 +252,12 @@ Function FightingEntropy
         Save()
         {
             $O = 1
-            ForEach ( $Object in $This.Classes, $This.Control, $This.Functions, $This.Graphics )
+            ForEach ( $Object in $This.Classes, 
+                                 $This.Control, 
+                                 $This.Functions, 
+                                 $This.Graphics )
             {
-                $Type = $This.Names[$O-1]
+                $Type = "Classes Control Functions Graphics".Split(" ")[$O-1]
                 Write-Host "Saving $Type [$O/4]"
                 $I = 1
 
@@ -161,76 +289,87 @@ Function FightingEntropy
                 $Module   += "# </{0}/{1}>" -f $_.Type, $_.Name
             }
 
-            $Module       += "# </Classes>"
-            $Module       += "# <Functions>"
+                $Module       += "# </Classes>"
+                $Module       += "# <Functions>"
 
-            $This.Functions  | % { 
+                $This.Functions  | % { 
 
-                $Module   += "# <{0}/{1}>" -f $_.Type, $_.Name
-                $Module   += "# {0}" -f $_.Path
-                $Module   += $_.Content
-                $Module   += "# </{0}/{1}>" -f $_.Type, $_.Name
+                    $Module   += "# <{0}/{1}>" -f $_.Type, $_.Name
+                    $Module   += "# {0}" -f $_.Path
+                    $Module   += $_.Content
+                    $Module   += "# </{0}/{1}>" -f $_.Type, $_.Name
+                }
+
+                $Module       += "# </Functions>"
+                $Module         += "Write-Theme `"Loaded Module [+] FightingEntropy [$($This.Version)]`" 10,3,15,0"
+
+                $This.Default    = $Env:PSModulePath -Split ";" | ? { $_ -match "Program Files" }
+                $This.Main       = $This.Default,"FightingEntropy" -join "\"
+                $This.Trunk      = $This.Main, $This.Version -join "\"
+                $This.ModPath    = $This.Trunk, "FightingEntropy.psm1" -join "\"
+                $This.ManPath    = $This.Trunk, "FightingEntropy.psd1" -join "\"
+
+                If ( !(Test-Path $This.Main))
+                {
+                    New-Item $This.Main -ItemType Directory -Verbose
+                }
+
+                If ( !(Test-Path $This.Trunk))
+                {
+                    New-Item $This.Trunk -ItemType Directory -Verbose
+                }
+
+                Set-Content -Path $This.ModPath -Value $Module -Verbose
+
+                $Item                    = @{  
+
+                    GUID                 = $This.GUID
+                    Path                 = $This.ManPath
+                    ModuleVersion        = $This.Version
+                    Copyright            = "(c) 2021 mcc85sx. All rights reserved."
+                    CompanyName          = "Secure Digits Plus LLC" 
+                    Author               = "mcc85sx / Michael C. Cook Sr."
+                    Description          = "Beginning the fight against Identity Theft, and Cybercriminal Activities"
+                    RootModule           = $This.ModPath   
+                }
+
+                New-ModuleManifest @Item
             }
 
-            $Module       += "# </Functions>"
-            $Module         += "Write-Theme `"Loaded Module [+] FightingEntropy [$($This.Version)]`" 10,3,15,0"
-
+        _Module([String]$Version)
+        {
+            $This.Version    = $Version
+            $This.Path       = $Env:ProgramData,$This.Company,$This.Name,$Version -join "\"
+            $This.Registry   = [_Registry]::New($This.Company,$This.Name,$Version,$This.OS.Type)
             $This.Default    = $Env:PSModulePath -Split ";" | ? { $_ -match "Program Files" }
             $This.Main       = $This.Default,"FightingEntropy" -join "\"
-            $This.Trunk      = $This.Main, $This.Version -join "\"
+            $This.Trunk      = $This.Main, $Version -join "\"
             $This.ModPath    = $This.Trunk, "FightingEntropy.psm1" -join "\"
             $This.ManPath    = $This.Trunk, "FightingEntropy.psd1" -join "\"
 
-            If ( !(Test-Path $This.Main))
+            $This._Build()
+
+            ForEach ( $Name in "Classes","Control","Functions","Graphics" )
             {
-                New-Item $This.Main -ItemType Directory -Verbose
-            }
+                $_Path = "$($This.Path)\$Name"
 
-            If ( !(Test-Path $This.Trunk))
-            {
-                New-Item $This.Trunk -ItemType Directory -Verbose
-            }
-
-            Set-Content -Path $This.ModPath -Value $Module -Verbose
-
-            $Item                    = @{  
-
-                GUID                 = $This.GUID
-                Path                 = $This.ManPath
-                ModuleVersion        = $This.Version
-                Copyright            = "(c) 2021 mcc85sx. All rights reserved."
-                CompanyName          = "Secure Digits Plus LLC" 
-                Author               = "mcc85sx / Michael C. Cook Sr."
-                Description          = "Beginning the fight against Identity Theft, and Cybercriminal Activities"
-                RootModule           = $This.ModPath   
-            }
-
-            New-ModuleManifest @Item
-        }
-
-        _Install()
-        {
-            $This.Build()
-
-            ForEach ( $Name in $This.Names )
-            {
-                $Path = "$($This.Root)\$Name"
-
-                If (!(Test-Path $Path))
+                If (!(Test-Path $_Path))
                 {
-                    New-Item $Path -ItemType Directory -Verbose
+                    New-Item $_Path -ItemType Directory -Verbose
                 }
 
-                $This.$Name = $This.Hash.$Name | % { [_File]::New($_,$Name,$Path) }
+                $This.$Name = $This.Manifest.$Name | % { [_File]::New($_,$Name,$_Path) }
             }
 
             $This.Gather()
             $This.Save()
             $This.Write()
+
+            $This.Tree = Get-ChildItem $This.Path -Recurse
         }
     }
 
-    [_Install]::New()
+    [_Module]::New("2021.6.0")
 
     [Net.ServicePointManager]::SecurityProtocol = $SecurityProtocol
 }
