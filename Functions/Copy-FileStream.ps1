72e776d3c4ea0b52375e45d389520bab4a4c5bb3
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-08-29 12:55:15 -0400

    Update Copy-FileStream.ps1

--- Functions/Copy-FileStream.ps1
+++ Functions/Copy-FileStream.ps1
@@ -22,7 +22,7 @@ Function Copy-FileStream # Renamed - https://stackoverflow.com/questions/2434133
             $This.Source      = [System.IO.File]::OpenRead($Source)
             $This.Destination = [System.IO.File]::OpenWrite($Destination)
 
-            Write-Progress -Activity "Copying file" -status "$Source -> $Destination" -PercentComplete 0
+            Write-Progress -Activity "Copying File" -Status "$Source -> $Destination" -PercentComplete 0
             Try 
             {
                 $This.Buffer  = [Byte[]]::New(4096)
@@ -34,7 +34,7 @@ Function Copy-FileStream # Renamed - https://stackoverflow.com/questions/2434133
                     $This.Total += $This.Count
                     If ($This.Total % 1mb -eq 0) 
                     {
-                        Write-Progress -Activity "Copying file" -status "$Source -> $Destination" -PercentComplete ([long]($This.Total * 100 / $This.Source.Length))
+                        Write-Progress -Activity "Copying File" -Status "$Source -> $Destination" -PercentComplete ([Long]($This.Total * 100 / $This.Source.Length))
                     }
                 } 
                 While ($This.Count -gt 0)
@@ -43,7 +43,7 @@ Function Copy-FileStream # Renamed - https://stackoverflow.com/questions/2434133
             {
                 $This.Source.Dispose()
                 $This.Destination.Dispose()
-                Write-Progress -Activity "Copying file" -Status "Ready" -Completed
+                Write-Progress -Activity "Copying File" -Status "Ready" -Completed
             }
         }
     }
