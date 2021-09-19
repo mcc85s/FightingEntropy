4054a08c36eef96ea59aa8ea4833c9b254460511
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-09-19 17:05:35 -0400

    Create Get-PSDModule.ps1

---
+++ Functions/Get-PSDModule.ps1
@@ -0,0 +1,36 @@
+Function Get-PSDModule
+{
+    $MDTDir = Get-ItemProperty "HKLM:\Software\Microsoft\Deployment 4" | % Install_Dir | % TrimEnd \
+    $Branch = "https://github.com/mcc85sx/FightingEntropy/blob/master/PSD"
+    $PSD    = "$MDTDir\PSD" 
+
+    If ($MDTDir -ne $Null)
+    {    
+        If ((Get-ChildItem $PSD -EA 0).Count -eq 0)
+        {
+            # Download/Extract/Delete the zip file
+            irm https://github.com/FriendsOfMDT/PSD/archive/refs/heads/master.zip?raw=true -OutFile "$PSD.zip" -Verbose
+            Expand-Archive -Path "$PSD.zip" -DestinationPath $MDTDir -Force
+            Remove-Item "$PSD.zip" -Confirm:$False -Force
+            Rename-Item "$PSD-master" -Newname $PSD -Verbose
+
+            $Branch = "https://github.com/mcc85sx/FightingEntropy/blob/master/PSD"
+            
+            # [Install-PSD.ps1]
+            irm "$Branch/Install-PSD.ps1?raw=true" -Outfile "$PSD\Install-PSD.ps1" -Verbose
+
+            # [PSDBackground.bmp]
+            irm "$Branch/PSDBackground.bmp?raw=true" -Outfile "$PSD\Branding\PSDBackground.bmp" -Verbose
+
+            # [PSDUtility.psm1]
+            irm "$Branch/PSDUtility.psm1?raw=true" -Outfile "$PSD\Scripts\PSDUtility.psm1" -Verbose
+
+            # [Set-ScreenResolution.ps1]
+            irm "$Branch/Set-ScreenResolution.ps1?raw=true" -Outfile "$PSD\Scripts\Set-ScreenResolution.ps1" -Verbose
+
+            # [PSDWizard.xaml]
+            irm "$Branch/PSDWizard.xaml?raw=true" -Outfile "$PSD\Scripts\PSDWizard.xaml" -Verbose
+        }
+    }
+    $PSD
+}
