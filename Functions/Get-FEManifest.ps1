b831a298d15c0b371a5b929125f1bf3f7f235e9b
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-07 09:00:15 -0400

    Update Get-FEManifest.ps1

--- Functions/Get-FEManifest.ps1
+++ Functions/Get-FEManifest.ps1
@@ -3,13 +3,10 @@ Function Get-FEManifest
     Class _Manifest
     {
         [String[]]      $Names = "Classes","Control","Functions","Graphics"
-
         [String[]]    $Classes = @(("FirewallRule Drive Drives ViperBomb File Cache Icons",
                 "Shortcut Brand Branding DNSSuffix DomainName ADLogin ADConnection ADReplication FEDCPromo Certificate Company Key RootVar Share Source",
                 "Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS Image Images Updates DCFound LocaleList LocaleItem" -join ' ') -Split " " | % { "_$_.ps1" })
-
         [String[]]    $Control = "Computer.png DefaultApps.xml header-image.png MDT_LanguageUI.xml zipcode.txt $( "FE","MDT","PSD" | % { "$_`Client","$_`Server" } | % { "$_`Mod.xml" } )" -Split " "
-
         [String[]]  $Functions = ("Add-ACL","Complete-IISServer","Export-Ini","Get-FECertificate","Get-DiskInfo","Get-FEDCPromo","Get-FEDCPromoProfile",
                 "Get-FEHive",
                 "Get-FEHost",
@@ -20,6 +17,7 @@ Function Get-FEManifest
                 "Get-FEModule",
                 "Get-FENetwork",
                 "Get-FEOS",
+                "Get-FEProcess",
                 "Get-FEService",
                 "Get-FEShare",
                 "Get-MadBomb","Get-MDTModule","Get-ServerDependency","Get-ViperBomb","Get-XamlWindow","Import-FEImage","Install-FEModule",
@@ -27,9 +25,7 @@ Function Get-FEManifest
                 "Show-ToastNotification","Update-FEShare","Write-Theme","Get-MDTOData","New-FEDeploymentShare","Start-VMGroup",
                 "Install-VMGroup","Get-FESiteMap","Invoke-KeyEntry","Copy-FileStream","Get-EnvironmentKey","Get-FEImageManifest","Invoke-cimdb",
                 "Set-ScreenResolution","Get-PSDModule","New-FEInfrastructure" | % { "$_.ps1" })
-
         [String[]]   $Graphics = "background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp sdplogo.png" -Split " "
-
         _Manifest()
         {
 
