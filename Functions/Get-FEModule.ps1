fc4e05f08fdc3428f6cd5f1d40d06d5a64a0e460
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-07 15:19:13 -0400

    Update Get-FEModule.ps1

--- Functions/Get-FEModule.ps1
+++ Functions/Get-FEModule.ps1
@@ -8,47 +8,12 @@ Function Get-FEModule
         [Parameter(ParameterSetName = "Functions" )][Switch] $Functions , 
         [Parameter(ParameterSetName =   "Control" )][Switch]   $Control , 
         [Parameter(ParameterSetName =  "Graphics" )][Switch]  $Graphics , 
-        [Parameter(ParameterSetName =      "Role" )][Switch]      $Role ,
-        [Parameter(ParameterSetName =       "GUI" )][Switch]       $GUI )
-        
-    Class _Process
-    {
-        [String]      $NPM
-        [String]       $PM
-        [String]       $WS
-        [String]      $CPU
-        [String]       $ID
-        [String]       $SI
-        [String]     $Name
-        _Process([Object]$Process)
-        {
-            $This.NPM  = "{0:n2}" -f ($Process.NonpagedSystemMemorySize/1KB)
-            $This.PM   = "{0:n2}" -f ($Process.PagedMemorySize/1MB)
-            $This.WS   = "{0:n2}" -f ($Process.WorkingSet/1MB)
-            $This.CPU  = "{0:n2}" -f $Process.TotalProcessorTime.TotalSeconds
-            $This.ID   = $Process.Id
-            $This.SI   = $Process.SessionID
-            $This.Name = $Process.ProcessName
-        }
-        [String] X([UInt32]$Width,[Object]$Value)
-        {
-            If ( $Value.Length -le 0 )
-            {
-                $Value = "0"
-            }
+        [Parameter(ParameterSetName =      "Role" )][Switch]      $Role )
 
-            Return ("[{0}{1}]" -f ( @(" ") * ( $Width - $Value.Length ) -join '' ), $Value)
-        }
-        [String] ToString()
-        {
-            Return @($This.X(10,$This.NPM),$This.X(10,$This.PM),$This.X(10,$This.WS),
-                     $This.X(10,$This.CPU),$This.X(6,$This.ID),$This.X( 6,$This.SI),
-                     $This.X(44,$This.Name))
-        }
-    }
-        
     Class _Module
     {
+        Hidden [Object] $Order = @(("Base Name Description Author Company Copyright GUID Version Date OS Manifest RegPath Default Main " +
+                                   "Trunk ModPath ManPath Tree Classes Control Functions Graphics Status") -Split " ")
         [String]        $Base
         [String]        $Name
         [String] $Description
@@ -75,8 +40,6 @@ Function Get-FEModule
         [Object]      $Status
         [Object]        $Type
         [Object]        $Role
-        Hidden [Object] $Report
-        Hidden [Object] $Line = (@(" ")*120 -join '')
         _Module([Object]$ID)
         {
             $This.Base        = $ID.Base
@@ -106,387 +69,44 @@ Function Get-FEModule
             $This.Type        = $This.OS.Type
             $This.Role        = Get-FERole
         }
-        Section([String]$Label)
+        [String] ModPrompt()
         {
-            Write-Host " "
-            Write-Host (@("-")*120 -join '')
-            Write-Host "[$Label]"
-            Write-Host (@("-")*120 -join '')
-            Write-Host " "
-        }  
-        HostInfo()
-        {
-            Write-Theme "Host info"
-            Write-Host $This.Line
-            
-            $This.Report.HostInfo = @( )
-
-            ForEach ( $Item in "Name DNS NetBIOS Hostname Username Principal IsAdmin Caption Version Build ReleaseID Code SKU Chassis" -Split " " )
-            {
-                $Info = ("{0}{1}: {2}" -f $Item, (@(" ")*(20-$Item.Length) -join ''), $This.Role.$Item)
-                Write-Host $Info
-                $This.Report.HostInfo += $Info
-            }
-
-            Write-Host " "
-        } 
-        ProcessInfo()
-        {
-            Write-Theme "Process info"
-            Write-Host $This.Line
-            
-            $This.Report.ProcessInfo = @( )
-
-                Write-Host "[     NPM] [      PM] [      WS] [       CPU] [     ID] [  SI] [                        ProcessName]"
-            Write-Host (@("-") * 120 -join '' )
-            
-            Get-Process | % { 
-                
-                $Info = [_Process]::New($_)
-                Write-Host $Info.ToString()
-                $This.Report.ProcessInfo += $Info
-            }
-            
-            Write-Host " "
-        }
-        NetInterfaceInfo()
-        {
-            Write-Theme "Network interface(s)"
-            Write-Host $This.Line
-            
-            $This.Report.NetInterfaceInfo = @( )
-
-            ForEach ( $Interface in $This.Role.Network.Interface )
-            {
-                Write-Host "[ ($($Interface.Description)) ]"
-                Write-Host " "
-                ForEach ( $Item in "Hostname Alias Index Description Status MacAddress Vendor" -Split " " )
-                {
-                    Write-Host ("{0}{1}: {2}" -f $Item, (@(" ")*(20-$Item.Length) -join ''), $Interface.$Item)
-                }
-                Write-Host " "
-            }
-            Write-Host " "
-        }
-        NetActiveInfo()
-        {
-            Write-Theme "Active interface(s)"
-            Write-Host $This.Line
-            
-            ForEach ( $Interface in $This.Role.Network.Active )
-            {
-                Write-Host "[ ($($Interface.Description)) ]"
-                Write-Host " "
-                ForEach ( $Item in "Hostname Alias Index Description Status MacAddress Vendor" -Split " " )
-                {
-                    Write-Host ("{0}{1}: {2}" -f $Item, (@(" ")*(20-$Item.Length) -join ''), $Interface.$Item)
-                }
-                Write-Host " "
-            }
-            Write-Host " "
-        }
-        NetStatInfo()
-        {
-            Write-Theme "Connection statistics"
-            Write-Host $This.Line
-            
-            $This.Report.NetstatInfo = @( )
-
-            $This.Role.Network.RefreshNetStat()
-            Write-Host "[ Proto] [                LocalAddress] [    LPort] [               RemoteAddress] [    RPort] [     State] [ Direction]"
-            $This.Role.Network.Netstat | % { 
-                
-                $0 = (@(" ")*(7-$_.Protocol.ToString().Length) -join '')
-                $1 = (@(" ")*(45-$_.LocalAddress.ToString().Length) -join '')
-                $2 = (@(" ")*(6-$_.LocalPort.ToString().Length) -join '')
-                $3 = (@(" ")*(45-$_.RemoteAddress.ToString().Length) -join '')
-                $4 = (@(" ")*(6-$_.RemotePort.ToString().Length) -join '')
-                $5 = (@(" ")*(12-$_.State.ToString().Length) -join '')
-                $6 = (@(" ")*(12-$_.Direction.ToString().Length) -join '')
-
-                $Info = ("$0{0} $1{1} $2{2} $3{3} $4{4} $5{5} $6{6}" -f $_.Protocol,$_.LocalAddress,$_.LocalPort,$_.RemoteAddress,$_.RemotePort,$_.State,$_.Direction)
-                Write-Host $Info
-                $This.Report.NetStatInfo += $Info
-            }
-            Write-Host " "
-        }
-        NetHostmapInfo()
-        {
-            Write-Theme "Network host(s)"
-            Write-Host $This.Line
-            
-            $This.Report.NetHostmapInfo      = @( )
-            ForEach ( $X in 0..($This.Role.Network.Hostmap.Count - 1) )
-            { 
-                $Item = $This.Role.Network.Hostmap[$X]
-                If (!$Item.Hostname)
-                {
-                    $Item.Hostname = "<host unknown/unnamed>"
-                }
-                
-                $Info = ("{0}{1}: {2}" -f (@(" ")*(30-$Item.IPAddress.ToString().Length) -join ''), $Item.IPAddress, $Item.Hostname)
-                Write-Host $Info
-                $This.Report.NetHostmapInfo += $Info
-            }
-        }
-        ServiceInfo()
-        {
-            Write-Theme "Service info"
-            Write-Host $This.Line
-            
-            $This.Report.ServiceInfo         = @( )
-
-            $This.Role.Service.Output        | % {
-
-                $IDName = $_.Name
-                $IDInfo = $_.DisplayName
-
-                If ( $IDName.Length -gt 44 )
-                {
-                    $IDName = "$($IDName[0..41] -join '')..."
-                }
-
-                If ( $IDInfo.Length -gt 44 )
-                {
-                    $IDInfo = "$($IDInfo[0..41] -join '')..."
-                }
-
-                $0 = (@(" ")*( 4-$_.Index.ToString().Length) -join "")
-                $1 = (@(" ")*(45- $IDName.Length) -join "")
-                $2 = (@(" ")* 2 -join "")
-                $3 = (@(" ")*(8-$_.StartMode.ToString().Length) -join "")
-                $4 = (@(" ")*(8-$_.State.ToString().Length) -join "")
-                $5 = (@(" ")*(45-$IDInfo.Length) -join "")
-
-                $Info = ("[$0{0}] $1{1} $2{2} $3{3} $4{4} $5{5}" -f $_.Index, $IDName, [Int]$_.Scope, $_.StartMode, $_.State, $IDInfo)
-                Write-Host $Info
-                $This.Report.ServiceInfo += $Info
-            }
-        }
-        Prime()
-        {                
-            Write-Theme "Priming FEModule API [(Processes, Network, Services)]"
-            Write-Host $This.Line
-
-            $This.Report         = @{
-
-                HostInfo         = @( )
-                ProcessInfo      = @( )
-                NetInterfaceInfo = @( )
-                NetActiveInfo    = @( )
-                NetStatInfo      = @( )
-                NetHostmapInfo   = @( )
-                ServiceInfo      = @( )
-            }
-                
-            Write-Host "[~] Processes [~]"
-            $This.Role.GetProcesses()
-
-            Write-Host "[~] Network [~]"
-            $This.Role.GetNetwork()
-
-            Write-Host "[~] Services [~]"
-            $This.Role.GetServices()
-
-            $This.HostInfo()
-            Start-Sleep 1
-            
-            $This.ProcessInfo()
-            Start-Sleep 1
-            
-            $This.NetInterfaceInfo()
-            Start-Sleep 1
-
-            $This.NetActiveInfo()
-            Start-Sleep 1
-            
-            $This.NetStatInfo()
-            Start-Sleep 1
-            
-            $This.NetHostmapInfo()
-            Start-Sleep 1
-            
-            $This.ServiceInfo()
-            Start-Sleep 1
-            
-            Write-Host " "
-        }
-    }
-        
-    Class _NVList
-    {
-        [String]$Name
-        [String]$Value
-        _NVList([String]$Name,[String]$Value)
-        {
-            $This.Name  = $Name
-            $This.Value = $Value
-        }
-    }
-        
-    Class _GUI
-    {
-        [Object] $Window
-        [Object]     $IO
-        [Object] $Module
-        _GUI([String]$RegPath)
-        {
-            $This.Window                 = Get-XamlWindow -Type FEModule
-            $This.IO                     = $This.Window.IO
-            $This.IO.Title               = "[FightingEntropy({0})][(2021.6.0)]" -f [Char]960
-            $This.Module                 = [_Module]::New((Get-ItemProperty $RegPath))
-
-            $This.Prime()
+            Return @( "[FightingEntropy($([char]960))][$($This.Version)]" )
         }
         ModuleInfo()
         {
-            $This.IO.ModuleInfo.ItemsSource = @( )
-
-            ForEach ( $Item in "Base Name Description Author Company Copyright GUID Version Date".Split(" ") )
-            {
-                $This.IO.ModuleInfo.ItemsSource += [_NVList]::New($Item,$This.Module.$Item)
-            }
+            Write-Theme $This -Title Module -Prompt $This.ModPrompt()
         }
-        OSInfo()
+        HostInfo()
         {
-            $This.IO.OSInfo.ItemsSource = @( )
-
-            ForEach ( $Item in "Ver Major Type" -Split " " )
-            {
-                $This.IO.OSInfo.ItemsSource += [_NVList]::New($Item,$This.Module.OS.$Item)
-            }
-
-            $This.IO.OSInfo.ItemsSource | ? Name -match Ver | % { $_.Name = "PSVersion" }
-        }
-        ManifestInfo()
-        {
-            ForEach ( $Item in "Classes Control Functions Graphics" -Split " " )
-            {
-                $This.IO."Manifest$Item".ItemsSource  = @( )
-
-                ForEach ( $Object in $This.Module.Manifest.$Item )
-                {
-                    $This.IO."Manifest$Item".ItemsSource += [_NVList]::New($Object,"$($This.Module.Base)/$Item/$Object")
-                }
-            }
-        }
-        RegistryInfo()
-        {
-            $This.IO.RegistryInfo.ItemsSource = @( )
-            $Reg = Get-ItemProperty $This.Module.RegPath
-
-            ForEach ( $Item in "Base Name Description Author Company Copyright GUID Version Date RegPath Default Main Trunk ModPath ManPath Path Status Type" -Split " ")
-            {
-                $This.IO.RegistryInfo.ItemsSource += [_NVList]::New($Item,$Reg.$Item)
-            }
-        }
-        TreeInfo()
-        {
-            ForEach ( $Item in "Classes Control Functions Graphics" -Split " " )
-            {
-                $This.IO."Tree$Item".ItemsSource  = @( )
-
-                ForEach ( $Object in $This.Module.$Item )
-                {
-                    $This.IO."Tree$Item".ItemsSource += $Object
-                }
-            }
-        }
-        RoleInfo()
-        {
-            $This.IO.RoleInfo.ItemsSource = @( )
-
-            ForEach ( $Item in "Name DNS NetBIOS Hostname Username Principal IsAdmin Caption Version Build ReleasedID Code SKU Chassis" -Split " " )
-            {
-                $This.IO.RoleInfo.ItemsSource += [_NVList]::New($Item,$This.Module.Role.$Item)
-            }
-        }
+            Write-Theme $This.Role -Title Host -Prompt $This.ModPrompt()
+        } 
         ProcessInfo()
-        {
-            $This.IO.ProcessInfo.ItemsSource = @( )
-
-            ForEach ( $Item in Get-Process | % { [_Process]::New($_) } )
-            {
-                $This.IO.ProcessInfo.ItemsSource += $Item
-            }
+        {            
+            Write-Theme (Get-FEProcess -Text) -Title Processes -Prompt $This.ModPrompt()
         }
-        InterfaceListInfo()
+        AdapterInfo()
         {
-            $This.IO.InterfaceList.ItemsSource = @( )
-
-            ForEach ( $Item in $This.Module.Role.Network.Interface )
-            {
-                $This.IO.InterfaceList.ItemsSource += $Item
-            }
+            # Write-Theme (Get-FENetwork2 -Adapter -Text) -Title Adapters -Prompt $This.ModPrompt()
         }
-        ActiveListInfo()
+        InterfaceInfo()
         {
-            $This.IO.ActiveList.ItemsSource = @( )
-
-            ForEach ( $Item in $This.Module.Role.Network.Active )
-            {
-                $This.IO.ActiveList.ItemsSource += $Item
-            }
+            # Write-Theme (Get-FENetwork2 -Interface -Text) -Title Interfaces -Prompt $This.ModPrompt()
         }
-        ConnectionListInfo()
+        ServiceInfo()
         {
-            $This.IO.ConnectionList.ItemsSource = @( )
-
-            ForEach ( $Item in $This.Module.Role.Network.Netstat )
-            {
-                $This.IO.ConnectionList.ItemsSource += $Item
-            }
-        }
-        HostListInfo()
-        {
-            $This.IO.HostList.ItemsSource = @( )
-
-            ForEach ( $Item in $This.Module.Role.Network.Hostmap )
-            {
-                $This.IO.HostList.ItemsSource += $Item
-            }
-        }
-        ServiceListInfo()
-        {
-            $This.IO.ServiceList.ItemsSource = @( ) 
-
-            ForEach ( $Item in $This.Module.Role.Service.Output )
-            {
-                $This.IO.ServiceList.ItemsSource += $Item
-            }
-        }
-        Prime()
-        {
-            $This.Module.Role | % { 
-
-                $_.GetProcesses()
-                $_.GetNetwork()
-                $_.GetServices()
-            }
-
-            $This.ModuleInfo()
-            $This.OSInfo()
-            $This.ManifestInfo()
-            $This.RegistryInfo()
-            $This.TreeInfo()
-            $This.RoleInfo()
-            $This.ProcessInfo()
-            $This.InterfaceListInfo()
-            $This.ActiveListInfo()
-            $This.ConnectionListInfo()
-            $This.HostListInfo()
-            $This.ServiceListInfo()
+            Write-Theme (Get-FEService -Text) -Title Services -Prompt $This.ModPrompt()
         }
     }
 
-    Class Version
+    Class _Version
     {
         [UInt32] $Exists
         [UInt32] $Year
         [UInt32] $Month
         [UInt32] $Slot
         [String] $RegPath
-        Version([Object]$Object)
+        _Version([Object]$Object)
         {
             $ID           = $Object.PSChildName.Split(".")
             $This.Year    = $ID[0]
@@ -515,7 +135,7 @@ Function Get-FEModule
         Throw "Registry not found"
     }
     
-    $Child     = Get-ChildItem $Default | % { [Version]::New($_) } | ? Exists | Sort-Object Year | Select-Object -First 1
+    $Child     = Get-ChildItem $Default | % { [_Version]::New($_) } | ? Exists | Sort-Object Year | Select-Object -First 1
     $RegPath   = Get-ItemProperty $Child.RegPath
     
     Switch($PSCmdLet.ParameterSetName)
@@ -526,6 +146,5 @@ Function Get-FEModule
         Control   {[_Module]::New($RegPath).Control}
         Graphics  {[_Module]::New($RegPath).Graphics}
         Role      {[_Module]::New($RegPath).Role}
-        GUI       {[_GUI]::New($RegPath.RegPath).Window.Invoke()}
     }
 }
