47565d6e3224617a0699ebeb305689c94a25037f
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-07 08:50:11 -0400

    Create Get-FEProcess.ps1

---
+++ Functions/Get-FEProcess.ps1
@@ -0,0 +1,71 @@
+Function Get-FEProcess
+{
+    [CmdLetBinding(DefaultParameterSetName=0)]
+    Param([Parameter(ParameterSetName=1,Mandatory)][Switch]$Output)
+    
+    Class FEProcess
+    {
+        [String]      $NPM
+        [String]       $PM
+        [String]       $WS
+        [String]      $CPU
+        [String]       $ID
+        [String]       $SI
+        [String]     $Name
+        FEProcess([Object]$Process)
+        {
+            $This.NPM  = "{0:n2}" -f ($Process.NonpagedSystemMemorySize/1KB)
+            $This.PM   = "{0:n2}" -f ($Process.PagedMemorySize/1MB)
+            $This.WS   = "{0:n2}" -f ($Process.WorkingSet/1MB)
+            $This.CPU  = "{0:n2}" -f $Process.TotalProcessorTime.TotalSeconds
+            $This.ID   = $Process.Id
+            $This.SI   = $Process.SessionID
+            $This.Name = $Process.ProcessName
+        }
+    }
+
+    Class FEProcesses
+    {
+        [Object]$Output
+        FEProcesses()
+        {
+            $This.Output = @( Get-Process | % { [FeProcess]$_ })
+        }
+        [String] Buffer([String]$Type,[String]$String)
+        {
+            $Buffer = Switch ($Type)
+            {
+                NPM  { 10 }
+                PM   { 10 }
+                WS   { 10 }
+                CPU  { 10 }
+                ID   { 10 }
+                SI   {  5 }
+            }
+            Return @( $String, (" " * ($Buffer - $String.Length) -join '') -join '')
+        }
+        [String[]] ToString()
+        {
+            Return @(
+                "NPM       PM        WS        CPU       ID        SI   Name                           "
+                "---       --        --        ---       --        --   ----                           "
+                ForEach ($Item in $This.Output)
+            {
+                $This.Buffer("NPM",$Item.NPM),
+                $This.Buffer( "PM",$Item.PM),
+                $This.Buffer( "WS",$Item.WS),
+                $This.Buffer("CPU",$Item.CPU),
+                $This.Buffer( "ID",$Item.ID),
+                $This.Buffer( "SI",$Item.SI),
+                $Item.Name -join ''
+            })
+        }
+    }
+
+    $Object = [FeProcesses]::New()
+    Switch($PSCmdLet.ParameterSetName)
+    {
+        0 { $Object.Output }
+        1 { $Object.ToString() }
+    }
+}
