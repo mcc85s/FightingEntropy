b62d5da9ec59b8ce2dc99caccb346034155566fc
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-17 09:10:18 -0400

    Update Get-FEManifest.ps1

--- Functions/Get-FEManifest.ps1
+++ Functions/Get-FEManifest.ps1
@@ -1,39 +1,20 @@
 Function Get-FEManifest
 {
-    Class _Manifest
+    Class Manifest
     {
         [String[]]      $Names = "Classes","Control","Functions","Graphics"
-        [String[]]    $Classes = @(("FirewallRule Drive Drives ViperBomb File Cache Icons",
-                "Shortcut Brand Branding DNSSuffix DomainName ADLogin ADConnection ADReplication FEDCPromo Certificate Company Key RootVar Share Source",
-                "Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS Image Images Updates DCFound LocaleList LocaleItem" -join ' ') -Split " " | % { "_$_.ps1" })
-        [String[]]    $Control = "Computer.png success.png failure.png DefaultApps.xml header-image.png MDT_LanguageUI.xml zipcode.txt $( "FE","MDT","PSD" | % { "$_`Client","$_`Server" } | % { "$_`Mod.xml" } )" -Split " "
-        [String[]]  $Functions = ("Add-ACL","Complete-IISServer","Export-Ini","Get-FECertificate","Get-DiskInfo",
-        "Get-FEADLogin",
-        "Get-FEDCPromo",
-        "Get-FEDCPromoProfile",
-                "Get-FEHive",
-                "Get-FEHost",
-                "Get-FEImage",
-                "Get-FEManifest",
-                "Get-FERole",
-                "Get-FEInfo",
-                "Get-FEModule",
-                "Get-FENetwork",
-                "Get-FEOS",
-                "Get-FEProcess",
-                "Get-FEService",
-                "Get-FEShare",
-                "Get-MadBomb","Get-MDTModule","Get-ServerDependency","Get-ViperBomb","Get-XamlWindow","Import-FEImage","Install-FEModule",
-                "Install-IISServer","New-ACLObject","New-Company","New-EnvironmentKey","New-FEImage","New-FEShare","Remove-FEModule","Remove-FEShare",
-                "Show-ToastNotification","Update-FEShare","Write-Theme","Get-MDTOData","New-FEDeploymentShare","Start-VMGroup",
-                "Install-VMGroup","Get-FESiteMap","Invoke-KeyEntry","Copy-FileStream","Get-EnvironmentKey","Get-FEImageManifest","Invoke-cimdb",
-                "Set-ScreenResolution","Get-PSDModule","New-FEInfrastructure" | % { "$_.ps1" })
+        [String[]]    $Classes = @("FirewallRule Drive Drives ViperBomb File Cache Icons Shortcut".Split(" ") | % { "_$_.ps1" })
+        [String[]]    $Control = "Computer.png success.png failure.png DefaultApps.xml header-image.png MDT_LanguageUI.xml vendorlist.txt zipcode.txt $( "FE","MDT","PSD" | % { "$_`Client","$_`Server" } | % { "$_`Mod.xml" } )" -Split " "
+        [String[]]  $Functions = ("Copy-FileStream","Get-DiskInfo","Get-EnvironmentKey","Get-FEADLogin","Get-FEDCPromo","Get-FEHost","Get-FEImageManifest",
+                                  "Get-FEInfo","Get-FEManifest","Get-FEModule","Get-FENetwork","Get-FEOS","Get-FEProcess","Get-FERole","Get-FEService",
+                                  "Get-MadBomb","Get-MDTModule","Get-PSDModule","Get-ViperBomb","Get-XamlWindow","Install-FEModule","Install-IISServer",
+                                  "Invoke-cimdb","Invoke-KeyEntry","New-EnvironmentKey","New-FEDeploymentShare","New-FEInfrastructure","Remove-FEModule",
+                                  "Set-ScreenResolution","Show-ToastNotification","Write-Theme" | % { "$_.ps1" })
         [String[]]   $Graphics = "background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp sdplogo.png" -Split " "
-        _Manifest()
+        Manifest()
         {
 
         }
     }
-
-    [_Manifest]::New()
+    [Manifest]::New()
 }
