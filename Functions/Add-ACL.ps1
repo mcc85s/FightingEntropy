b6a832324ef6b800f57cbe8bf7380c761a337965
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-17 08:43:58 -0400

    Delete Add-ACL.ps1

--- Functions/Add-ACL.ps1
+++
@@ -1,9 +0,0 @@
-Function Add-ACL
-{
-    [CmdLetBinding()]Param(
-        [Parameter(Mandatory)][String]$Path,
-        [Parameter(Mandatory)][System.Security.AccessControl.FileSystemAccessRule] $ACL
-    )
-
-    Get-ACL -Path $Path | % AddAccessRule $ACL | % { Set-ACL -Path $Path -AclObject $_ }
-}
