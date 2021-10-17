55fd81e0e801d36fd215bf2fa55bfd29b6457ad0
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-17 16:09:22 -0400

    Update Get-FEManifest.ps1

--- Functions/Get-FEManifest.ps1
+++ Functions/Get-FEManifest.ps1
@@ -8,7 +8,7 @@ Function Get-FEManifest
         [String[]]  $Functions = ("Copy-FileStream","Get-DiskInfo","Get-EnvironmentKey","Get-FEADLogin","Get-FEDCPromo","Get-FEHost","Get-FEImageManifest",
                                   "Get-FEInfo","Get-FEManifest","Get-FEModule","Get-FENetwork","Get-FEOS","Get-FEProcess","Get-FERole","Get-FEService",
                                   "Get-MadBomb","Get-MDTModule","Get-PSDModule","Get-ViperBomb","Get-XamlWindow","Install-FEModule","Install-IISServer",
-                                  "Invoke-cimdb","Invoke-KeyEntry","New-EnvironmentKey","New-FEDeploymentShare","New-FEInfrastructure","Remove-FEModule",
+                                  "Invoke-cimdb","Invoke-KeyEntry","New-EnvironmentKey","New-FEInfrastructure","Remove-FEModule",
                                   "Set-ScreenResolution","Show-ToastNotification","Write-Theme" | % { "$_.ps1" })
         [String[]]   $Graphics = "background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp sdplogo.png" -Split " "
         Manifest()
