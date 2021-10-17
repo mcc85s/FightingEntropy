7aa4ac3dda06d7e51c6847511f2c62ddc2af1eb3
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-17 16:21:01 -0400

    Update Get-EnvironmentKey.ps1

--- Functions/Get-EnvironmentKey.ps1
+++ Functions/Get-EnvironmentKey.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.LINK
+
+.NOTES
+          FileName: Get-EnvironmentKey.ps1
+          Solution: FightingEntropy Module
+          Purpose: For retrieving and instantiating an environment key (branding, icons, certificates, etc)
+          Author: Michael C. Cook Sr.
+          Contact: @mcc85s
+          Primary: @mcc85s
+          Created: 2021-10-09
+          Modified: 2021-10-17
+          
+          Version - 2021.10.0 - () - Finalized functional version 1.
+
+          TODO:
+
+.Example
+#>
 Function Get-EnvironmentKey
 {
     [CmdLetBinding()]Param(
