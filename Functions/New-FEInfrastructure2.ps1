5e376667f0b75c22e05944accbd2db59da816bb1
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-30 10:50:38 -0400

    Update New-FEInfrastructure2.ps1

--- Functions/New-FEInfrastructure2.ps1
+++ Functions/New-FEInfrastructure2.ps1
@@ -1367,7 +1367,7 @@ Function Sitemap
     [Sitemap]::New()
 }
 
-Function AddsNode
+Function AddsController
 {
     Class Topology
     {
@@ -1422,13 +1422,13 @@ Function AddsNode
         }
     }
 
-    Class AddsNode
+    Class AddsController
     {
         [Object] $Gateway
         [Object] $Server
         [Object] $Workstation
         [Object] $Object
-        AddsNode()
+        AddsController()
         {
             $This.Gateway     = @( )
             $This.Server      = @( )
@@ -1527,16 +1527,17 @@ Function AddsNode
             }
         }
     }
-    [AddsNode]::New()
+    [AddsController]::New()
 }
 
-Function VmNode
+Function VmController
 {
     [CmdLetBinding()]
     Param(
         [Parameter()][String]$HostName = "localhost",
         [Parameter()][PSCredential]$Credential
     )
+
     Class Topology
     {
         [String] $Organization
@@ -1561,6 +1562,7 @@ Function VmNode
         [String] $Parent
         [String] $DistinguishedName
         [UInt32] $Exists
+        [Object] $VMInstance
         Topology([Object]$Node,[Object]$VM)
         {
             $This.Organization      = $Node.Organization
@@ -1584,6 +1586,7 @@ Function VmNode
             $This.ReverseDNS        = $Node.ReverseDNS
             $This.DistinguishedName = $Node.DistinguishedName
             $This.Parent            = $Node.Parent
+            $This.VMInstance        = $VM
         }
     }
 
@@ -1592,7 +1595,7 @@ Function VmNode
         [String] $Type
         [String] $Name
         [Bool]   $Create
-        Select([Object]$Item)
+        Select([Object]$Type,[Object]$Item)
         {
             $This.Type   = $Item.Type
             $This.Name   = $Item.Name
@@ -1600,82 +1603,114 @@ Function VmNode
         }
     }
 
-    Class Test
+    Class VmSwitchNode
     {
-        [Object] $Name
-        [Bool] $Exists
-        Test([String]$Type,[String]$Name)
+        [String] $Name
+        [String] $ID
+        [String] $Type
+        [String] $Description
+        [UInt32] $Exists
+        VmSwitchNode([String]$Name,[String]$Type)
         {
-            $Return      = $Null
-            $This.Name   = $Name
-            If ($Type -eq "VM")
+            $This.Name        = $Name
+            $This.Type        = $Type
+            $This.Exists      = 0
+        }
+        VmSwitchNode([Object]$Switch)
+        {
+            $This.Name        = $Switch.Name
+            $This.ID          = $Switch.ID
+            $This.Type        = $Switch.SwitchType
+            $This.Description = @($Switch.NetAdapterInterfaceDescription,"-")[$Switch.NetAdapterInterfaceDescription -ne ""]
+            $This.Exists      = 1
+        }
+        New()
+        {
+            If (Get-VMSwitch -Name $This.Name)
             {
-                $Return  = Get-VM -Name $Name -EA 0
+                Throw "Switch already exists"
             }
-            If ($Type -eq "Switch")
+
+            $Switch           = New-VMSwitch -Name $This.Name -Type $This.Type -Verbose
+            
+            $This.ID          = $Switch.GUID
+            $This.Description = @($Switch.NetAdapterInterfaceDescription,"-")[$Switch.NetAdapterInterfaceDescription -ne ""]
+            $This.Exists      = 1
+        }
+        Remove()
+        {
+            If (!$This.Exists)
             {
-                $Return  = Get-VMSwitch -Name $Name -EA 0
+                Throw "Switch does not exist"
             }
-            $This.Exists = !!$Return
+
+            Get-Switch -Name $This.Name | Remove-VMSwitch -Force -Verbose -Confirm:$False
         }
     }
 
-    Class VMSwitch
+    Class VmObjectNode
     {
-        [String] $Type = "Switch"
-        [Object] $Name
-        [Bool] $Create = $True
-        VMSwitch([String]$Name)
-        {
-            $This.Name = $Name
-        }
-    }
-
-    Class VMObject
-    {
-        [Object]$Item
-        [Object]$Name
-        [Double]$MemoryStartupBytes
-        [Object]$Path
-        [Object]$NewVHDPath
-        [Double]$NewVHDSizeBytes
-        [Object]$Generation
-        [Object]$SwitchName
-        VMObject([Object]$Item,[UInt32]$Mem,[UInt32]$HD,[UInt32]$Gen,[String]$Switch)
+        [Object]         $Item
+        [Object]         $Name
+        [Double]       $Memory
+        [Object]         $Path
+        [Object]          $Vhd
+        [Double]      $VhdSize
+        [Object]   $Generation
+        [UInt32]         $Core
+        [Object[]] $SwitchName
+        [UInt32]       $Exists
+        VmObjectNode([Object]$Item,[UInt32]$Memory,[UInt32]$HDD,[UInt32]$Generation,[UInt32]$Core,[String]$Switch,[String]$Path)
         {
             $This.Item               = $Item
             $This.Name               = $Item.Name
-            $This.MemoryStartupBytes = ([UInt32]$Mem)*1048576
-            $This.Path               = "{0}\$($Item.Name).vmx"
-            $This.NewVhdPath         = "{0}\$($Item.Name).vhdx"
-            $This.NewVhdSizeBytes    = ([UInt32]$HD)*1073741824
-            $This.Generation         = $Gen
-            $This.SwitchName         = $Switch
+            $This.Memory             = ([UInt32]$Memory)*1MB
+            $This.Path               = "$Path\$($Item.Name)"
+            $This.Vhd                = "$Path\$($Item.Name)\$($Item.Name).vhdx"
+            $This.VhdSize            = ([UInt32]$HDD)*1GB
+            $This.Generation         = $Generation
+            $This.Core               = $Core
+            $This.SwitchName         = @($Switch)
+            $This.Exists             = 0
         }
-        New([Object]$Path)
+        VmObjectNode([Object]$VM)
         {
-            If (!(Test-Path $Path))
+            $This.Name               = $Vm.Name
+            $This.Memory             = $Vm.MemoryStartup
+            $This.Path               = $Vm.Path
+            $This.Vhd                = $Vm.HardDrives[0].Path
+            $This.VhdSize            = $This.Vhd | Get-VHD | % Size
+            $This.Generation         = $Vm.Generation
+            $This.Core               = $Vm.ProcessorCount
+            $This.SwitchName         = $Vm.NetworkAdapters.SwitchName
+            $This.Exists             = 1
+        }
+        New()
+        {
+            If (Get-VM -Name $This.Name)
+            {
+                Throw "This VM already exists"
+            }
+
+            If (!(Test-Path $This.Path))
             {
                 Throw "Invalid path"
             }
 
-            $This.Path             = $This.Path -f $Path
-            $This.NewVhdPath       = $This.NewVhdPath -f $Path
-
             $Object                = @{
 
                 Name               = $This.Name
-                MemoryStartupBytes = $This.MemoryStartupBytes
+                MemoryStartupBytes = $This.Memory
                 Path               = $This.Path
-                NewVhdPath         = $This.NewVhdPath
-                NewVhdSizeBytes    = $This.NewVhdSizebytes
+                NewVhdPath         = $This.Vhd
+                NewVhdSizeBytes    = $This.VhdSize
                 Generation         = $This.Generation
                 SwitchName         = $This.SwitchName
             }
 
             New-VM @Object -Verbose
-            $Ct = @{Gateway=1;Server=2;Workstation=2}[$This.Item.Type]
-            Set-VMProcessor -VMName $This.Name -Count $Ct -Verbose
+            $This.Exists           = 1
+            Set-VMProcessor -VMName $This.Name -Count $This.Core -Verbose
         }
         Start()
         {
@@ -1684,6 +1719,9 @@ Function VmNode
         Remove()
         {
             Get-VM -Name $This.Name | Remove-VM -Force -Confirm:$False -Verbose
+            $This.Exists           = 0
+            Remove-Item $This.Vhd -Force -Verbose -Confirm:$False
+            Remove-Item $This.Path -Force -Recurse -Verbose -Confirm:$False
         }
         Stop()
         {
@@ -1703,7 +1741,7 @@ Function VmNode
         }
     }
 
-    Class VmNode
+    Class VmController
     {
         [String]   $HostName
         [String]     $Status
@@ -1711,18 +1749,18 @@ Function VmNode
         [String] $Credential
         [Object]       $Host
         [Object]     $Switch
-        [Object]    $Control
         [Object]   $Internal
         [Object]   $External
-        [Object]     $ADNode
-        [Object]     $VMNode
-        VmNode([String]$ID)
+        [Object]   $AddsNode
+        [Object]     $VmNode
+        [Object]    $VmStack
+        VmController([String]$ID)
         {
             $This.Username   = $Env:Username
             $This.Credential = Get-Credential $Env:Username
             $This.GetHost($ID)
         }
-        VmNode([String]$ID,[PSCredential]$Credential)
+        VmController([String]$ID,[PSCredential]$Credential)
         {
             $This.Username   = $Credential.Username
             $This.Credential = $Credential
@@ -1742,31 +1780,75 @@ Function VmNode
                 $This.Switch   = $Null
                 $This.External = $Null
                 $This.Internal = $Null
+                $This.AddsNode = @( )
+                $This.VmNode   = @( )
+                $This.VmStack  = @( )
             }
             If ($This.Status -eq "Running")
             {
+                Write-Host "Loading [~] [Virtual Host: $($This.Hostname)]"
                 $This.Host     = Get-VMHost   -ComputerName $This.Hostname
-                $This.Switch   = Get-VMSwitch -ComputerName $This.Hostname
-                $This.External = $This.Switch | ? SwitchType -eq External
-                $This.Internal = $This.Switch | ? SwitchType -eq Internal
+
+                $This.Switch   = @( )
+                ForEach ($Item in Get-VMSwitch -ComputerName $This.Hostname)
+                {
+                    Write-Host "Loading [~] [Virtual Switch: $($Item.Name)]"
+                    $This.Switch += [VmSwitchNode]::New($Item) 
+                }
+
+                $This.External = $This.Switch | ? Type -eq External
+                $This.Internal = $This.Switch | ? Type -eq Internal
+                $This.AddsNode = @( )
+                $This.VmNode   = @( )
+                ForEach ($Item in Get-Vm -ComputerName $This.Hostname)
+                {
+                    Write-Host "Loading [~] [Virtual Machine: $($Item.Name)]"
+                    $This.VmNode += [VmObjectNode]::New($Item)
+                }
+                $This.VmStack  = @( )
             }
         }
-        GetVmNode([Object]$DistinguishedName)
+        LoadAddsNode([Object]$AddsNode)
         {
-
+            $This.AddsNode += $AddsNode   
         }
-        AddVmNode([Object]$AddsNode)
+        [Object] GetVmObjectNode([String]$Name)
         {
-
+            Return @( $This.VmNode | ? Name -eq $Name )
+        }
+        AddVmObjectNode([Object]$AddsNode,[UInt32]$Memory,[UInt32]$HDD,[UInt32]$Generation,[UInt32]$Core,[String]$SwitchName,[String]$Path)
+        {
+            $Item = $This.GetVMNode($AddsNode.Name)
+            If ($Item)
+            {
+                Write-Host "Item [!] Exists [$($Item.Name)]" -F 12
+            }
+            If (!$Item)
+            {
+                $This.VmNode += [VMObjectNode]::New($AddsNode,$Memory,$HDD,$Generation,$Core,$SwitchName,$Path)
+            }
+        }
+        GetVmNodeList()
+        {
+            $This.VmStack = @( )
+            ForEach ($Item in $This.VmNode)
+            {
+                If ($Item.Name -in $This.AddsNode.Name)
+                {
+                    $ADDS          = $This.AddsNode | ? Name -eq $Item.Name 
+                    $Item.Item     = $ADDS.Type
+                    $This.VmStack += [Topology]::New($ADDS,$Item)
+                }
+            }
         }
     }
     If ($Credential)
     {
-        [VmNode]::New($Hostname,$Credential)
+        [VmController]::New($Hostname,$Credential)
     }
     If (!$Credential)
     {
-        [VmNode]::New($Hostname)
+        [VmController]::New($Hostname)
     }
 }
 
@@ -2556,27 +2638,36 @@ Class ModuleFile
 Class Main
 {
     [Object]            $Module = (Get-FEModule)
+    [Object]        $Connection
+    [String]      $Organization
+    [String]        $CommonName
+    [Object]        $Credential
     Static [String]       $Base = "$Env:ProgramData\Secure Digits Plus LLC\FightingEntropy"
     Static [String]        $GFX = ("{0}\Graphics"    -f [Main]::Base)
     Static [String]       $Icon = ("{0}\icon.ico"    -f [Main]::GFX)
     Static [String]       $Logo = ("{0}\OEMLogo.bmp" -f [Main]::GFX)
     Static [String] $Background = ("{0}\OEMbg.jpg"   -f [Main]::GFX)
-    [String]      $Organization
-    [String]        $CommonName
-    [Object]        $Credential
     [Object]            $System
     [Object]            $Config
     [Object]          $SiteList
     [Object]       $NetworkList
     [Object]           $Sitemap
-    [Object]          $AddsNode
-    [Object]            $VmNode
+    [Object]    $AddsController
+    [Object]      $VmController
     [Object]             $Image
     [Object]            $Update
     [Object]               $MDT
     [Object]               $WDS
     Main()
     {
+        $This.Connection      = Get-FEADLogin
+        If (!$This.Connection)
+        {
+            Write-Error "Could not log into server"
+            Break
+        }
+        $This.Credential      = $This.Connection.Credential
+
         # Pulls system information
         $This.Module.Role.GetSystem()
 
@@ -2601,12 +2692,12 @@ Class Main
         $This.Sitemap          = Sitemap
 
         # AD Node factory
-        $This.AddsNode         = AddsNode
+        $This.AddsController   = AddsController
 
         # VM Node factory
         If ($This.Config.HyperV)
         {
-            $This.VmNode       = VmNode
+            $This.VmController = VmController -Hostname localhost -Credential $This.Credential
         }
 
         # Imaging
@@ -2647,15 +2738,23 @@ $Main.Sitemap.SetSiteLinkBridge($Main.Sitemap.Sitelink[0].DistinguishedName)
 $Main.Sitemap.LoadSitemap()
 $Main.Sitemap.GetSitemap()
 
-# AD Node
 # [Gateway]
 ForEach ($Site in $Main.Sitemap.Aggregate)
 {
-    $Main.AddsNode.AddAddsNode("Gateway",$Site.Name,$Site)
+    $Main.AddsController.AddAddsNode("Gateway",$Site.Name,$Site)
 }
 
 # [Server]
 ForEach ($Site in $Main.Sitemap.Aggregate)
 {
-    $Main.AddsNode.AddAddsNode("Server","dc1-$($Site.Postal)",$Site)
+    $Main.AddsController.AddAddsNode("Server","dc1-$($Site.Postal)",$Site)
 }
+
+# [Send AddsController nodes to VmController]
+ForEach ($Item in $Main.AddsController.Object)
+{
+    $Main.VmController.LoadAddsNode($Item)
+}
+
+# Populate VMController VMStack
+$Main.VmController.GetVMNodeList()
