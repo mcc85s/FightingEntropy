8b5b2e3e61920147703b6a0adc7f5ef42cd13e6f
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-10-31 21:48:18 -0400

    Update Get-FEADLogin.ps1

--- Functions/Get-FEADLogin.ps1
+++ Functions/Get-FEADLogin.ps1
@@ -13,7 +13,7 @@
           Contact: @mcc85s
           Primary: @mcc85s
           Created: 2021-10-09
-          Modified: 2021-10-30
+          Modified: 2021-10-31
           
           Version - 2021.10.0 - () - Finalized functional version 1.
 
