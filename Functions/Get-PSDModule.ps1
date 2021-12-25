2b671638f67d8fbb60e8171a6a3ba934106d6e08
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2021-12-25 16:33:38 -0500

    Update Get-PSDModule.ps1

--- Functions/Get-PSDModule.ps1
+++ Functions/Get-PSDModule.ps1
@@ -8,24 +8,22 @@
 .NOTES
           FileName: Get-PSDModule.ps1
           Solution: FightingEntropy Module
-          Purpose: Retrieves the PowerShell Deployment modification by FriendsOfMDT and the FightingEntropy customizations
+          Purpose: Retrieves the PowerShell Deployment modification by FriendsOfMDT and inserts FightingEntropy customizations
           Author: Michael C. Cook Sr.
           Contact: @mcc85s
           Primary: @mcc85s
           Created: 2021-10-09
-          Modified: 2021-10-17
+          Modified: 2021-12-25
           
           Version - 2021.10.0 - () - Finalized functional version 1.
-
-          TODO: 10/17/2021 - Integrate with the Get-MDTModule function
-
 .Example
 #>
 Function Get-PSDModule
 {
-    $MDTDir = Get-ItemProperty "HKLM:\Software\Microsoft\Deployment 4" | % Install_Dir | % TrimEnd \
-    $Branch = "https://github.com/mcc85sx/FightingEntropy/blob/master/PSD"
-    $PSD    = "$MDTDir\PSD"
+    $MDTDir  = Get-ItemProperty "HKLM:\Software\Microsoft\Deployment 4" | % Install_Dir | % TrimEnd \
+    $PSD     = "$MDTDir\PSD"
+    $Branch1 = "https://gitub.com/mcc85s/FightingEntropy/blob/master/PSD"
+    $Branch2 = "https://github.com/mcc85s/FightingEntropy/blob/main"
 
     If ($MDTDir)
     {    
@@ -37,26 +35,32 @@ Function Get-PSDModule
             Remove-Item "$PSD.zip" -Confirm:$False -Force
             Rename-Item "$PSD-master" -Newname $PSD -Verbose
             
-            # [Install-PSD.ps1]
-            Invoke-RestMethod "$Branch/Install-PSD.ps1?raw=true" -Outfile "$PSD\Install-PSD.ps1" -Verbose
-
-            # [PSDBackground.bmp]
-            Invoke-RestMethod "$Branch/PSDBackground.bmp?raw=true" -Outfile "$PSD\Branding\PSDBackground.bmp" -Verbose
-
+            # [Install-PSD.ps1] - Installs the modification for MDT (Branch 1)
+            Invoke-RestMethod "$Branch1/Install-PSD.ps1?raw=true" -Outfile "$PSD\Install-PSD.ps1" -Verbose
+            
+            # [PSDFinal.ps1] (Branch 2)
+            Invoke-RestMethod "$Branch2/PSDFinal.ps1?raw=true" -Outfile "$PSD\Scripts\PSDFinal.ps1" -Verbose
+            
+            # [PSDStart.ps1]
+            Invoke-RestMethod "$Branch2/PSDStart.ps1?raw=true" -Outfile "$PSD\Scripts\PSDStart.ps1" -Verbose
+            
+            # [PSDWizard.psm1]
+            Invoke-RestMethod "$Branch2/PSDWizard.psm1?raw=true" -Outfile "$PSD\Scripts\PSDWizard.psm1" -Verbose
+            
+            # [PSDDeploymentShare.psm1]
+            Invoke-RestMethod "$Branch2/PSDDeploymentShare.psm1?raw=true" -Outfile "$PSD\Scripts\PSDDeploymentShare.psm1" -Verbose
+            
             # [PSDUtility.psm1]
-            Invoke-RestMethod "$Branch/PSDUtility.psm1?raw=true" -Outfile "$PSD\Scripts\PSDUtility.psm1" -Verbose
+            Invoke-RestMethod "$Branch2/PSDUtility.psm1?raw=true" -Outfile "$PSD\Scripts\PSDUtility.psm1" -Verbose
+            
+            # [PSDGather.psm1]
+            Invoke-RestMethod "$Branch2/PSDGather.psm1?raw=true" -Outfile "$PSD\Scripts\PSDGather.psm1" -Verbose
+            
+            # [PSDBackground.bmp]
+            Invoke-RestMethod "$Branch2/PSDBackground.bmp?raw=true" -Outfile "$PSD\Branding\PSDBackground.bmp" -Verbose
 
             # [Set-ScreenResolution.ps1]
-            Invoke-RestMethod "$Branch/Set-ScreenResolution.ps1?raw=true" -Outfile "$PSD\Scripts\Set-ScreenResolution.ps1" -Verbose
-
-            # [PSDWizard.psm1]
-            Invoke-RestMethod "$Branch/PSDWizard.psm1?raw=true" -Outfile "$PSD\Scripts\PSDWizard.psm1" -Verbose
-
-            # [PSDWizard.xaml.Initialize.ps1]
-            Invoke-RestMethod "$Branch/PSDWizard.xaml.Initialize.ps1?raw=true" -Outfile "$PSD\Scripts\PSDWizard.xaml.Initialize.ps1" -Verbose
-
-            # [PSDWizard.xaml]
-            Invoke-RestMethod "$Branch/PSDWizard.xaml?raw=true" -Outfile "$PSD\Scripts\PSDWizard.xaml" -Verbose
+            Invoke-RestMethod "$Branch2/Set-ScreenResolution.ps1?raw=true" -Outfile "$PSD\Scripts\Set-ScreenResolution.ps1" -Verbose
         }
     }
     $PSD
