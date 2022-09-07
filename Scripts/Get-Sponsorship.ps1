c013c1a30c125a20367aa9228d141023aeabe7e6
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2022-09-07 11:37:34 -0400

    Update Get-Sponsorship.ps1

--- Scripts/Get-Sponsorship.ps1
+++ Scripts/Get-Sponsorship.ps1
@@ -1,1 +1,194 @@
+<#
+.SYNOPSIS
+        No one goes around and says "I really LOVE cybercriminals AND what they do."
+        Wanna know why...?
+        Uh, it's cause cybercriminals suck ass. That's the end of the conversation. 
+        Show people who the hell is really gonna roll up their sleeves, and do (2) things:
+        (1) ATTACK the CYBERCRIMINALS by using MANIPULATIVE OFFENSIVE STRATEGIES such as:
+            - calling them a bunch of douchebags repeatedly
+            - refining the insults even more, and then directing them at these pansies
+            - showing them what a waste of life they truly are, just like Vladimir Putin
+            - educating people on how lame (cybercriminals/Vladimir Putin) really (are/is)
+        (2) BUILDING a SERIES of EXTREMELY SOPHISTICATED DEFENSIVE TECHNIQUES such as:
+            - educating people on how lame cybercriminals really are
+            - manipulating these morons into waging their ATTACKS against PRIMED HONEYPOTS
+            - using VIRTUALIZATION to create PRIMED HONEYPOTS and developing PATTERN ANALYSIS
+            - studying the PATTERN ANALYSIS to EXFILTRATE the ATTACK VECTORS they use 
+            - continue making fun of them, and explaining HOW/WHY they're a waste of life 
+        ...and call these things [FightingEntropy(π)]
+.DESCRIPTION
+        Meant to impress the daylights out of the Cybersecurity Infrastructure Security Agency,
+        the National Security Agency, the Federal Bureau of Investigation, the Department of
+        Homeland Security, the Central Intelligence Agency, Barack Obama, Joe Biden, and etc.
+        Maybe even William Gates, Stephen Ballmer, Satya Nadella...
+.LINK
+        
+.NOTES
+          FileName : Get-Sponsorship.ps1
+          Solution : [FightingEntropy(π)] Module
+          Purpose  : For annoying and/or persuading people into making a (sponsorship/investment)
+          Author   : Michael C. Cook Sr.
+          Contact  : @mcc85s
+          Primary  : @mcc85s
+          Created  : 2022-09-07
+          Modified : 2022-09-07
+          
+          Version - 2021.10.0 - () - Finalized functional version 1.
+          TODO:
+.Example
+$Deck = Get-Sponsorship -Name "Obtaining a SPONSORSHOP/INVESTMENT"
 
+$Deck.Add(@(
+"Suppose I, Michael C. Cook Sr, wanted to get FUNDING for my COMPANY...",
+"Secure Digits Plus LLC...?",
+"    ",
+"Well...",
+"What could I DO, in order to PROVE,",
+"that I'm WORTH making a rather SIGNIFICANT INVESTMENT, of like, CAPITAL...?",
+"    ",
+"Ya know, so I don't have to add a MAKE A DONATION button to my work,",
+"Or like,",
+"HARASS PEOPLE or ANNOY the (FBI/NSA/DHS/CIA) for a fuckin' INVESTMENT...?",
+"    ",
+"Well, for starters, (1) idea comes to mind...",
+"    ",
+"Actually, a couple. Continuing to be a 100% smartass. And then...",
+"USING A PROGRAM I MADE TO COLLECT NEARBY WIFI RADIOS/NETWORKS",
+"Not unlike the CENTRAL INTELLIGENCE AGENCY TOOL, in VAULT 7, called 'ELSA'."))
+
+$Deck.Add(@(
+"So, take for instance this URL below...",
+"I'll chunk it out into 'VARIABLES' so that people will be able to SEE...",
+"this 'thing' I started a few years ago, called a GitHub project...",
+"...and with this 'GitHub' project, EVERYONE THAT HAS EVER KNOWN ME...",
+"...will be able to SEE for CERTAIN...",
+"...that I've been WORKING MY ASS OFF FOR THE LAST 3.5 YEARS...",
+"...so that I could have an EDGE over the fuckin' CYBERCRIMINALS.",
+"    ",
+"`$Repo  = 'github.com/mcc85s/FightingEntropy'",
+"`$Trunk = 'blob/main/Functions'",
+"`$Name  = 'Search-WirelessNetwork.ps1'",
+"    ",
+"Probably doesn't LOOK like a FULL URL quite yet...?",
+"That's cause... it needs to be CONCATENATED... As in, 'assembled'.",
+"`$URL = `$Repo, `$Trunk, `$Name -join '/'",
+"    ",
+"NOW, it'll look like this..."))
+
+$Deck.Add(@(
+"    ",
+"/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\",
+"    ",
+"PS C:\Users\mcadmin> `$URL",
+"    ",
+"github.com/mcc85s/FightingEntropy/blob/main/Functions/Search-WirelessNetwork.ps1",
+"    ",
+"\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/",
+"    ",
+"...and then we can LOAD THIS FUNCTION INTO a PowerShell v5.1 HOST LIKE SO...",
+"    ",
+"/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\",
+"    ",
+"PS C:\Users\mcadmin> Invoke-RestMethod `$URL?raw=true | Invoke-Expression",
+"    ",
+"\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/",
+"    "))
+
+$Deck.Add(@(
+"If you performed the above, and ANY ERRORS SHOW UP...?",
+"...it is because you haven't loaded the function...",
+"...using PowerShell v5.1 on Windows..."
+"...which has the Presentation Framework (for the GUI)...",
+"...and the Windows Runtime Extensions (for the Radios)...",
+"...otherwise, you should be able to perform the following:",
+"    ",
+"/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\",
+"    ",
+"PS C:\Users\mcadmin> Search-WirelessNetwork",
+"    ",
+"<PowerShell starts doin' some really cool shit in the background...>",
+"<A few seconds later, a GUI pops up with info that looks like this...>",
+"    ",
+"\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/¯¯\__/",
+"    "))
+
+$Deck.Draw()
+#>
+Function Get-Sponsorship
+{
+    [CmdLetBinding()]Param([Parameter(Mandatory)][String]$Name)
+
+    # // _____________________________________________________________________________________
+    # // | This will contain a single slide worth of messages to be processed by Write-Theme | 
+    # // ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+
+    Class SponsorshipSlide
+    {
+        [UInt32]        $Index
+        [String]         $Name
+        [String]        $Title
+        [Object]  $InputObject
+        SponsorshipSlide([UInt32]$Index,[String]$Name,[String[]]$InputObject)
+        {
+            $This.Index       = $Index
+            $This.Name        = $Name
+            $This.InputObject = $InputObject
+        }
+        SetTitle([UInt32]$Total)
+        {
+            $This.Title       = "{0} ({1}/{2})" -f $This.Name, ($This.Index+1), $Total
+        }
+        Draw()
+        {
+            Write-Theme -Title $This.Title -InputObject $This.InputObject
+        }
+        [String] ToString()
+        {
+            Return $This.Index
+        }
+    }
+
+    # // ___________________________________________________________________________
+    # // | This will contain a collection of slides to be processed by Write-Theme | 
+    # // ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+
+    Class SponsorshipContainer
+    {
+        [String] $Name
+        [Object] $Output
+        SponsorshipContainer([String]$Name)
+        {
+            $This.Name   = $Name
+            $This.Output = @( )
+        }
+        Add([String[]]$InputObject)
+        {
+            $This.Output += [SponsorshipSlide]::New($This.Output.Count,$This.Name,$InputObject)
+            $This.Rerank()
+        }
+        Rerank()
+        {
+            If ($This.Output.Count -eq 1)
+            {
+                $This.Output[0].SetTitle(1)
+            }
+            If ($This.Output.Count -gt 1)
+            {
+                $C = $This.Output.Count
+                ForEach ($X in 0..($C-1))
+                {
+                    $This.Output[$X].SetTitle($C)
+                }
+            }
+        }
+        Draw()
+        {
+            ForEach ($Slide in $This.Output)
+            {
+                $Slide.Draw()
+            }
+        }
+    }
+
+    [SponsorshipContainer]::New($Name)
+}
