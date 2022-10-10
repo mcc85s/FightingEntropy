d05e4c9d5d5a98e70e4781e821c746e05239ddd4
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2022-10-10 11:18:21 -0400

    Create Get-ControlExtension.ps1

---
+++ Functions/Get-ControlExtension.ps1
@@ -0,0 +1,67 @@
+<#
+.SYNOPSIS
+.DESCRIPTION
+.LINK
+.NOTES
+    ____                                                                                                    ________    
+   //¯¯\\__________________________________________________________________________________________________//¯¯\\__//   
+   \\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯\\   
+   //¯¯\\__[ [FightingEntropy()][2022.10.0] ]______________________________________________________________//¯¯\\__//   
+   \\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯\\   
+   //¯¯¯                                                                                                           //   
+   \\                                                                                                              \\   
+   //        FileName   : Get-ControlExtension.ps1                                                                 //   
+   \\        Solution   : [FightingEntropy()][2022.10.0]                                                           \\   
+   //        Purpose    : Extends the graphical user interface controls handed off to the threading                //   
+   \\                     dispatcher.                                                                              \\   
+   //        Author     : Michael C. Cook Sr.                                                                      //   
+   \\        Contact    : @mcc85s                                                                                  \\   
+   //        Primary    : @mcc85s                                                                                  //   
+   \\        Created    : 2022-10-10                                                                               \\   
+   //        Modified   : 2022-10-10                                                                               //   
+   \\        Demo       : N/A                                                                                      \\   
+   //        Version    : 0.0.0 - () - Finalized functional version 1.                                             //   
+   \\        TODO       : N/A                                                                                      \\   
+   //                                                                                                           ___//   
+   \\___                                                                                                    ___//¯¯\\   
+   //¯¯\\__________________________________________________________________________________________________//¯¯¯___//   
+   \\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯¯    
+    ¯¯¯\\__[ 2022-10-10 10:57:31    ]______________________________________________________________________//¯¯¯        
+        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯            
+.Example
+#>
+
+Function Get-ControlExtension
+{
+    [CmdLetBinding()]Param(
+    [Parameter(Mandatory)][UInt32]$Index,
+    [Parameter(Mandatory)][String]$Name,
+    [Parameter(Mandatory)][Object]$Control,
+    [Parameter(Mandatory)][String]$Type)
+
+    Class ControlExtension
+    {
+        [UInt32] $Index
+        [String] $Name
+        [String] $Type
+        [Object] $Control
+        [Object] $Dispatcher
+        [Object] $Thread
+        [Object] $Data
+        ControlExtension([UInt32]$Index,[String]$Name,[Object]$Control,[String]$Type)
+        {
+            $This.Index      = $Index
+            $This.Name       = $Name
+            $This.Type       = $Type
+            $This.Control    = $Control
+            $This.Dispatcher = $Control.Dispatcher
+            $This.Thread     = $Control.Dispatcher.Thread
+        }
+        [String] ToString()
+        {
+            Return $This.Control.ToString()
+        }
+    }
+
+    [ControlExtension]::New($Index,$Name,$Control,$Type)
+}
