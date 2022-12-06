9d60829bd0b7bc38bdeaada75a8e91c2c5d029ef
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2022-12-05 23:14:24 -0500

    Update DismLine.ps1

--- Scripts/DismLine.ps1
+++ Scripts/DismLine.ps1
@@ -1,108 +1,68 @@
-# Parses the DISM log properly, not QUITE done, but close enough
+# Parses the DISM logs
 Class DismLine
 {
-    [UInt32]    $Rank
-    [String]    $Line
-    DismLine([UInt32]$Rank,[String]$Line)
+    [UInt32] $Index
+    [UInt32] $Rank
+    [String] $Content
+    DismLine([UInt32]$Index,[Uint32]$Rank,[String]$Content)
     {
-        $This.Rank  = $Rank
-        $This.Line  = $Line
+        $This.Index   = $Index
+        $This.Rank    = $Rank
+        $This.Content = $Content
     }
     [String] ToString()
     {
-        Return $This.Line
+        Return $This.Content
     }
 }
 
 Class DismEntry
 {
-    Hidden [UInt32] $Index
-    Hidden [UInt32] $Total
-    Hidden [String]  $Line
-    [String]         $Date
-    [String]         $Type
-    Hidden [String]   $Com
-    [Object]      $Message
+    [UInt32]     $Index
+    [DateTime]    $Date
+    [Object]   $Content
     DismEntry([UInt32]$Index,[String]$Line)
     {
-        $This.Index       = $Index
-        $This.Line        = $Line
-        $This.Date        = ([DateTime]$This.Line.Substring(0,19)).ToString("MM/dd/yyyy HH:mm:ss")
-        $This.SetType()
-        $This.Message     = @( )
-        $This.AddLine($Line.Substring(50))
+        $This.Index   = $Index
+        $This.Date    = $Line.Substring(0,19)
+        $This.Content = @( )
+        $This.Add($Line.Substring(21))
     }
-    AddLine([String]$Line)
+    Add([String]$Line)
     {
-        $This.Message    += [DismLine]::New($This.Message.Count,$Line)
-    }
-    SetType()
-    {
-        $SetType          = $This.Line.Substring(21,29).Split(" ") | ? Length -gt 0
-        If ($SetType.Count -eq 1)
-        {
-            $This.Type    = $SetType
-        }
-        If ($SetType.Count -gt 1)
-        {
-            $This.Type    = $SetType[0]
-            $This.Com     = $SetType[1]
-        }
-    }
-    SetTotal([UInt32]$Total)
-    {
-        $This.Total = $Total
+        $This.Content += [DismLine]::New($This.Index,$This.Content.Count,$Line)
     }
 }
 
-Class DismLog
+Class DismFile
 {
-    [String]               $Path
-    Hidden [Object]     $Content
-    Hidden [Object]        $Swap = @{ }
-    [Object]             $Output
-    DismLog([String]$Path)
+    [String] $Path
+    [Object] $Output
+    DismFile()
     {
-        If (!(Test-Path $Path))
-        {
-            Throw "Invalid path"
-        }
-        $This.Path    = $Path
-        $This.Content = [System.IO.File]::ReadAllLines($Path)
-        $Count        = $This.Content.Count
-        $Depth        = ([String]$This.Content.Count).Length
-        $Start        = [DateTime]::Now
-        $Step         = [Math]::Round($Count/100)
-        $Slot         = 1..100 | % { $_ * $Step }
-        $Rank         = 0
+        $This.Path    = "$Env:Windir\Logs\CBS\CBS.log"
+        $Content      = [System.IO.File]::ReadAllLines($This.Path)
+        $This.Output  = @( )
 
-        Write-Progress -Activity Processing -Status ("({0:d$Depth}/{1}) 0.00% [ETA: <unknown>]" -f 0,$Count) -PercentComplete 0
-        ForEach ($X in 0..($This.Content.Count-1))
+        ForEach ($X in 0..($Content.Count-1))
         {
-            $Line = $This.Content[$X]
-            If ($Line -match "^\d{4}\-\d{2}\-\d{2}\s{1}\d{2}\:\d{2}\:\d{2}")
+            Switch -Regex ($Content[$X])
             {
-                $This.Swap.Add($This.Swap.Count,[DismEntry]::New($This.Swap.Count,$Line))
-            }
-            Else
-            {
-                $This.Swap[$This.Swap.Count-1].AddLine($Line)
-            }
-
-            If ($X -in $Slot)
-            {
-                $Rank ++
-                $Percent    = (($X*100)/$Count)
-                $Span       = [TimeSpan]([DateTime]::Now-$Start) | % TotalSeconds
-                $Remain     = [TimeSpan]::FromSeconds(($Span / $Percent) * (100-$Percent))
-                Write-Progress -Activity Processing -Status ("({0:d$Depth}/{1}) {2:n2}% [ETA: {3}]" -f $X,$Count,$Percent,$Remain) -PercentComplete 0
+                "^\d{4}\-\d{2}\-\d{2} \d{2}\:\d{2}\:\d{2}"
+                {
+                    $This.Add($Content[$X])
+                }
+                Default
+                {
+                    $This.Output[-1].Add($Content[$X])
+                }
             }
         }
-        Write-Progress -Activity Processing -Status ("({0}/{0}) 100.00%" -f $Count) -Complete
-
-        $This.Output = $This.Swap[0..($This.Swap.Count-1)]
+    }
+    Add([String]$Entry)
+    {
+        $This.Output += [DismEntry]::New($This.Output.Count,$Entry)
     }
 }
 
-$Path    = "C:\Windows\Logs\Dism\Dism.log"
-$Dism = [DismLog]::New($Path)
+$Dism = [DismFile]::New()
