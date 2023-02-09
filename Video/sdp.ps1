4e6c00f0795210488bc28007b0da245393aad1aa
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-02-09 09:28:38 -0500

    Create sdp.ps1

---
+++ Video/sdp.ps1
@@ -0,0 +1,110 @@
+$Ctrl = New-YouTubePorfolio -Name "Michael C. Cook" -Company "Secure Digits Plus LLC"
+
+("securedigitsplusllc3084","Secure Digits Plus LLC"),
+("michaelcook7389","Michael C. Cook"),
+("mykalcook","Michael Cook"),
+("securedigitsplus2892","Secure Digits Plus") | % { 
+
+    $Ctrl.Add($_[0],$_[1])
+}
+
+# 0 / [Secure Digits Plus LLC (securedigitsplusllc3084)]
+("02-07-2023","rastructure","17:25","zIDnV1BsiCA"),
+("02-07-2023","Golub Corp. - Empire State Plaza","01:59:05","Dj9E-eNe4Tg"),
+("02-04-2023","Why William Moak is stupid","27:53","aejBB77d_oA"),
+("02-04-2023","Write-Element (Demonstration)","03:04:48","j58BO1p_EJ8"),
+("02-01-2023","New-VmController [Flight Test v2.0] Part I","02:01:57","nqTOmNIilxw"),
+("01-12-2023","PowerShell | Virtualization Lab + FEDCPromo","01:14:43","9v7uJHF-cGQ"),
+("11-22-2022","Search-WirelessNetwork","01:15:56","Y7wEiuNJhN0"),
+("10-28-2022","[FightingEntropy()][2022.10.1]","01:22:26","S7k4lZdPE-I"),
+("10-15-2022","Intellectual Property Theft","13:33","-GScIS_PlOo"),
+("10-14-2022","Indicative Behaviors","15:03","ZiTVgtg68Jc"),
+("10-04-2022","God Mode Cursor","00:20","dU_5rdVkCD8"),
+("10-03-2022","Behavioral Analysis","31:32","xhMQbOoDvOc"),
+("09-29-2022","Write Progress Extension","02:15","K738wZSKcjo"),
+("09-26-2022","God Mode Cursor","01:12","tW80Zj_H6Fw"),
+("09-27-2022","Correlations","57:04","1ABQ6rfRg8Y"),
+("09-08-2022","Developing a GUI with style","02:11:03","z6_GeVYbcC4"),
+("08-11-2022","PowerShell Resume","05:18","QSuge7p5_I8"),
+("08-03-2022","Scribbles locking ANY word editor","08:43","vY2fIhS9ruo"),
+("04-15-2022","Event Logs Utility (Work in progress)","09:52:39","CCOTI6_Veoo"),
+("04-05-2022","Wireless Network Scanner Utility (PowerShell/Xaml)","05:18","35EabWfh8dQ"),
+("02-15-2022","A Matter of National Security","20:27","e4VnZObiez8"),
+("12-05-2021","[FightingEntropy(π)] FEInfrastructure Preview/Demo","01:49:57","6yQr06_rA4I"),
+("10-25-2021","Ambitious Automation","01:56:00","1E-3POI29Jo"),
+("10-14-2021","DCPromo and Networking","38:23","4q3fWhTOuFk"),
+("10-14-2021","FEInfrastructure (Alpha/Beta)","09:32","AdAilZe1EJ4"),
+("10-02-2021","Cross Platform Development","02:43:44","vbAH4l6Bm7A"),
+("09-23-2021","[FightingEntropy(π)]://FEWizard PowerShell Deployment feat. DVR","08:17","lZX5fAgczz0"),
+("09-16-2021","PowerShell Deployment Wizard (featuring DVR) [WIP]","04:05:38","m84TElKgAFg"),
+("09-10-2021","Resolve-LogAddressList","12:25","_i2bPQT1ZtU"),
+("05-23-2020","Virtual Tour","01:35:44","HT4p28bRhqc"),
+("01-25-2019","Computer Answers - MDT","32:39","5Cyp3pqIMRs") | % {
+
+    $Ctrl.AddVideo(0,$_[0],$_[1],$_[2],$_[3])
+}
+
+# 1 / [Michael C. Cook (michaelcook7389)]
+("06-29-2022","SCSO and NYSP trying to be sneaky","00:19","W0SZ9Iby3VY"),
+("08-30-2021","FEDeploymentShare Part 2","02:33:21","vg359UlYVp8"),
+("08-11-2021","New-FEDeploymentShare","10:44","70NJcRFTZo8"),
+("07-26-2021","FEDeploymentShare (Part 1)","38:48","eaAyCJ7zsZw"),
+("07-26-2021","2021 07 26 12 24 59","01:13:13","jmvzDV4RCQY"),
+("06-06-2021","[FightingEntropy(π)][(2021.6.0)]@[FEModule GUI (Part 2)]","01:17:43","9zjQqQ0sKPg"),
+("06-06-2021","[FightingEntropy(π)][(2021.6.0)]@[FEModule GUI (Part 1)]","02:42:22","Zc8Fq4E3Nc0"),
+("03-09-2021","A Deep Dive: Demonstration - Building XAML/WPF via PowerShell","57:16","NK4NuQrraCI"),
+("02-15-2021","FightingEntropy (π) Testing...","53:26","45U1qFlU_74"),
+("12-13-2020","About 'My Briefcase' from (Windows 95/98)","02:15","SmsqBv200B8"),
+("12-12-2020","PowerShell XAML/WPF Classes","01:05:26","51Ymdd9XqQ"),
+("11-07-2020","FightingEntropy - New Deployment Share GUI","11:40","RZ1SEUwSb9Q"),
+("11-05-2020","FightingEntropy - (Windows Image Extraction)","06:45","KI0-V5OGy5w"),
+("10-14-2020","Drafting and Design meets Programming","21:57","MRcLWQCJwt4"),
+("09-30-2020","NMap Vendor List","15:11","bOpZIeQF5KQ"),
+("09-16-2020","Write-Theme (Update/[FEObject])","16:00","dBpfRazxAi0"),
+("09-05-2020","(Cruise Control) 2/2","01:25:34","6SubeX4gvD0"),
+("09-05-2020","(Cruise Control) 1/2","56:36","afLnNLdG2PU"),
+("09-03-2020","FightingEntropy [FEModule/FERoot]","11:05","nFijaWGytnA"),
+("08-13-2020","Write-Theme & Show-ToastNotification (Source Code/Lesson Plan/Resume in desc.)","10:17","QGtzvsGSUak"),
+("08-09-2020","PowerShell/C# Classes","32:35","EQXRrPQ-iOw"),
+("05-21-2020","Service Configuration Tool","29:25","hbBVNerI_Qk"),
+("11-28-2019","Some Powershell scripting/programming methodologies","43:48","bZuSgBK36CE"),
+("08-28-2019","Education/Exhibition of Programming/Design/Engineering with Powershell/C#/.Net","01:47:16","v6RrrzR5v2E"),
+("05-28-2019","Hybrid | Desired State Controller | Process Exhibition (The long version)","01:02:43","C8NYaaqJAlI"),
+("03-24-2019","Hybrid(Server+Client)","47:24","qiZcHqkAzbs"),
+("08-19-2018","News 10 - Smart TV report","02:00","bPdWt7kcd3M"),
+("01-16-2018","Driving in Snow (feat. Audi A4 Quattro)","23:55","pi1DIQWuce8") | % { 
+
+    $Ctrl.AddVideo(1,$_[0],$_[1],$_[2],$_[3])
+}
+
+# 2 / [Michael Cook (mykalcook)]
+("08-13-2022","2018_1220-(OPNsense Configuration)","41:08","eD0-VQ2y_yg"),
+("09-08-2021","cimdb2","05:52","vA8_HLZ--mQ"),
+("06-05-2021","[Q3A] 20KDM2 - Return to Castle: Quake (2002)","05:16","xN53K9oGCME"),
+("06-05-2021","[Q3A] 20KDM1 - Tempered Graveyard (2001)","10:33","dyHwm9AdkQs"),
+("06-05-2021","[Q3A] 20KCTF1 - Out Of My Head (2002)","16:16","rwyHCNnwlkM"),
+("06-05-2021","[Q3A] 20KDM3 - Insane Products (2005)","11:25","EG8UyJSMK3Y"),
+("02-17-2021","2017_0831-(Threadripper Gen1)","01:02","ApEqOjAoAfM"),
+("10-23-2020","CentOS 7 Setup","01:08:20","iOKOkJJ1ZbQ"),
+("10-20-2020","2019_1004-AsusQ504UA301(tilted)","56:33","YDWm-f7WEWs"),
+("10-20-2020","Configuring pfSense/Hardened BSD","41:08","eYEESvPOWh4"),
+("10-20-2020","Network Troubleshooting","01:38","0nEiGijjOEY"),
+("10-20-2020","Spectrum Cable Modem Reset","03:34","LfZW-s0BMow"),
+("05-16-2019","Secure Digits Plus LLC @ Hybrid | Desired State Controller ( Beta Preview )","41:01","jplOHy_b1bA"),
+("03-02-2019","2019 03 01 23 35 05","01:01:30","RypW9xbClJo"),
+("07-28-2018","Trickshot - Wicked","09:55","joioy6bIyPE") | % { 
+
+    $Ctrl.AddVideo(2,$_[0],$_[1],$_[2],$_[3])
+}
+
+# 3 / [Secure Digits Plus (securedigitsplus2892)]
+("10-20-2021","[FightingEntropy]://Domain Controller Promotion","17:10","O8A2PDfQOBs"),
+("06-30-2021","PowerShell - (Xaml/WPF GUI) Windows Image Extraction","10:09","G10EuwlNAyo"),
+("06-27-2021","PowerShell - Advanced System Administration Lab","40:28","xgffIccX1eg"),
+("06-20-2021","Install-pfSense (Auto)","10:03","E_uFbzS0blQ"),
+("06-06-2021","[FightingEntropy(π)][(2021.6.0)] Installation + FEModule GUI","03:58","acYldTcKUAs"),
+("06-04-2021","[FightingEntropy(π)][(2021.6.0)]","07:05","UYnCj5Hcq6o"),
+("03-16-2021","FightingEntropy (π) v2021.3.1 | New-FEDeploymentShare GUI","01:38:47","4yFKeK9CzU0") | % { 
+
+    $Ctrl.AddVideo(3,$_[0],$_[1],$_[2],$_[3])
+}
