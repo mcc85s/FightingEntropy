4b972a6d08fb6d586564a44b3dfea9e1dcc8ea38
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-02-24 20:00:10 -0500

    Update New-TranscriptionCollection.ps1

--- Scripts/New-TranscriptionCollection.ps1
+++ Scripts/New-TranscriptionCollection.ps1
@@ -53,20 +53,22 @@ Function New-TranscriptionCollection
 
     Class TranscriptionEntry
     {
-        [UInt32]    $Index
-        [Object]    $Party
-        [String]     $Date
-        [String]     $Time
-        [String] $Position
-        [String]     $Type
-        [String]     $Note
-        TranscriptionEntry([UInt32]$Index,[Object]$Party,[Object]$Position,[String]$Note)
+        [UInt32]         $Index
+        [Object]         $Party
+        [String]          $Date
+        [String]          $Time
+        [String]      $Position
+        Hidden [String] $Length
+        [String]          $Type
+        [String]          $Note
+        TranscriptionEntry([UInt32]$Index,[Object]$Party,[Object]$Position,[String]$End,[String]$Note)
         {
             $This.Index    = $Index
             $This.Party    = $Party
             $This.Date     = $Position.Date
             $This.Time     = $Position.Time
             $This.Position = $Position.Position
+            $This.Length   = [TimeSpan]$End - $This.Position
             $This.Type     = Switch -Regex ($Note)
             {
                 "^\*{1}" { "Action"    }
@@ -130,9 +132,14 @@ Function New-TranscriptionCollection
         {
             Return [TranscriptionParty]::New($Index,$Name)
         }
-        [Object] TranscriptionEntry([UInt32]$Index,[Object]$Party,[Object]$Position,[String]$Note)
+        [Object] TranscriptionEntry([UInt32]$Index,[Object]$Party,[Object]$Position,[String]$End,[String]$Note)
         {
-            Return [TranscriptionEntry]::New($Index,$Party,$Position,$Note)
+            $xEnd = $This.Position($End)
+            Return [TranscriptionEntry]::New($Index,$Party,$Position,$xEnd,$Note)
+        }
+        Write([String]$String)
+        {
+            [Console]::WriteLine($String)
         }
         AddParty([String]$Name)
         {
@@ -142,50 +149,133 @@ Function New-TranscriptionCollection
             }
 
             $This.Party += $This.TranscriptionParty($This.Party.Count,$Name)
-            Write-Host "Party [+] [$Name] added."
+
+            $This.Write("Party [+] [$Name] added.")
         }
-        AddEntry([UInt32]$Index,[String]$Position,[String]$Note)
+        AddEntry([UInt32]$Index,[String]$Position,[String]$End,[String]$Note)
         {   
             If ($Index -gt $This.Party.Count)
             {
                 Throw "Party [!] [$Index] is out of bounds"
             }
 
-            $Person       = $This.Party[$Index]
-            $xTime        = $This.GetPosition($Position)
+            $This.Output += $This.TranscriptionEntry($This.Output.Count,
+                                                     $This.Party[$Index],
+                                                     $This.GetPosition($Position),
+                                                     $End,
+                                                     $Note)
 
-            $This.Output += [TranscriptionEntry]::New($This.Output.Count,$Person,$xTime,$Note)
-            Write-Host "Entry [+] [$($xTime.Position)] added"
+            $This.Write("Entry [+] [$($This.Output[-1].Position)] added")
         }
-        X([UInt32]$Index,[String]$Position,[String]$Note)
+        X([UInt32]$Index,[String]$Position,[String]$End,[String]$Note)
         {
-            $This.AddEntry($Index,$Position,$Note)
+            $This.AddEntry($Index,$Position,$End,$Note)
+        }
+        [String] ToString()
+        {
+            Return "({0}/{1})" -f $This.Date, $This.Name
+        }
+    }
+
+    Class TranscriptionHistoryItem
+    {
+        [UInt32]    $Index
+        [UInt32]     $File
+        [UInt32]     $Rank
+        [String]    $Party
+        [String] $Position
+        [TimeSpan] $Length
+        [String]     $Note
+        TranscriptionHistoryItem([UInt32]$Index,[UInt32]$File,[Object]$Item)
+        {
+            $This.Index    = $Index
+            $This.File     = $File
+            $This.Rank     = $Item.Index
+            $This.Party    = $Item.Party
+            $This.Position = $Item.Position
+            $This.Note     = $Item.Note
+        }
+    }
+
+    Class TranscriptionHistoryList
+    {
+        [Object] $Output
+        TranscriptionHistoryList()
+        {
+            $This.Output = @( )
+        }
+        [Object] TranscriptionHistoryItem([UInt32]$Index,[UInt32]$File,[Object]$Item)
+        {
+            Return [TranscriptionHistoryItem]::New($Index,$File,$Item)
+        }
+        Add([UInt32]$File,[Object]$Current)
+        {
+            $This.Output += $This.TranscriptionHistoryItem($This.Output.Count,$File,$Current.Output[-1])
         }
     }
 
     Class TranscriptionCollection
     {
-        [String] $Date
-        [String] $Name
-        [Object] $File
-        TranscriptionCollection([String]$Date,[String]$Name)
+        [String]           $Name
+        [String]           $Date
+        [Object]           $File
+        [Object]        $History
+        Hidden [Int32] $Selected
+        TranscriptionCollection([String]$Name,[String]$Date)
         {
-            $This.Date   = ([DateTime]$Date).ToString("MM/dd/yyyy")
-            $This.Name   = $Name
-            $This.File   = @( )
+            $This.Name     = $Name
+            $This.Date     = ([DateTime]$Date).ToString("MM/dd/yyyy")
+            $This.File     = @( )
+            $This.History  = $This.TranscriptionHistoryList()
+            $This.Selected = -1
         }
         [Object] TranscriptionFile([UInt32]$Index,[String]$Name,[String]$Date,[String]$Start,[String]$Length,[String]$Url)
         {
             Return [TranscriptionFile]::New($Index,$Name,$Date,$Start,$Length,$Url)
         }
-        [Object] Get([UInt32]$Index)
+        [Object] TranscriptionParty([UInt32]$Index,[String]$Name)
+        {
+            Return [TranscriptionParty]::New($Index,$Name)
+        }
+        [Object] TranscriptionEntry([UInt32]$Index,[Object]$Party,[Object]$Position,[String]$Note)
+        {
+            Return [TranscriptionEntry]::New($Index,$Party,$Position,$Note)
+        }
+        [Object] TranscriptionHistoryList()
+        {
+            Return [TranscriptionHistoryList]::New()
+        }
+        Write([String]$Line)
+        {
+            [Console]::WriteLine($Line)
+        }
+        Check([UInt32]$Index)
         {
             If ($Index -gt $This.File.Count)
             {
                 Throw "Invalid file index"
             }
+        }
+        [Object] Get([UInt32]$Index)
+        {
+            $This.Check($Index)
 
             Return $This.File[$Index]
+        }
+        [Object] Current()
+        {
+            If ($This.Selected -eq -1)
+            {
+                Throw "File not selected"
+            }
+
+            Return $This.File[$This.Selected]
+        }
+        Select([UInt32]$Index)
+        {
+            $This.Check($Index)
+
+            $This.Selected = $Index
         }
         AddFile([String]$Name,[String]$Date,[String]$Start,[String]$Duration,[String]$Url)
         {
@@ -195,11 +285,51 @@ Function New-TranscriptionCollection
             $Out += "Added [+] File     : [{0}]" -f $Item.Name
             $Out += "          Date     : [{0}]" -f $Item.Date
             $Out += "          Duration : [{0}]" -f $Item.Duration
-            $Out += "          Url      : [{0}]" -f $Item.Url 
+            $Out += "          Url      : [{0}]" -f $Item.Url
+            $Out += " "
             
-            $Out | % { [Console]::WriteLine($_) }
+            $Out | % { $This.Write($_) }
 
             $This.File += $Item
+        }
+        AddParty([String]$Name)
+        {
+            $Current = $This.Current()
+
+            If ($Name -in $Current.Party.Name)
+            {
+                Throw "Party [!] [$Name] already specified"
+            }
+
+            $Current.Party += $This.TranscriptionParty($Current.Party.Count,$Name)
+
+            $This.Write("Party [+] [$Name] added.")
+        }
+        AddEntry([UInt32]$Index,[String]$Position,[String]$Note)
+        {
+            $Current = $This.Current()
+
+            If ($Index -gt $Current.Party.Count)
+            {
+                Throw "Party [!] [$Index] is out of bounds"
+            }
+
+            $Current.Output += $This.TranscriptionEntry($Current.Output.Count,
+                                                        $Current.Party[$Index],
+                                                        $Current.GetPosition($Position),
+                                                        $Note)
+
+            $This.History.Add($This.Selected,$Current)
+
+            $This.Write("Entry [+] [$($Current.Output[-1].Position)] added")
+        }
+        X([UInt32]$Index,[String]$Position,[String]$Note)
+        {
+            $This.AddEntry($Index,$Position,$Note)
+        }
+        [String] ToString()
+        {
+            Return "({0}) <TranscriptionCollection>" -f $This.File.Count
         }
     }
 
@@ -208,5 +338,5 @@ Function New-TranscriptionCollection
         $Date = [DateTime]::Now.ToString("MM/dd/yyyy")
     }
 
-    [TranscriptionCollection]::New($Date,$Name)
+    [TranscriptionCollection]::New($Name,$Date)
 }
