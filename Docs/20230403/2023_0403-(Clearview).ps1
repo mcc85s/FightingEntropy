4b667b1a4899d4521d1546c5a262587a2e9aefea
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-04-03 16:28:05 -0400

    Add files via upload

---
+++ Docs/20230403/2023_0403-(Clearview).ps1
@@ -0,0 +1,28 @@
+$File = 
+[The Future of Artificial Intelligence]
+Those who do not remember the past, are condemned to repeat it. -George Santanaya
+
+I don't understand why people have a hard time wrapping their heads around the dangers of artificial intelligence. It's like, it thinks automatically. It does everything automatically. It doesn't need to ask for input. It thinks for itself. If it sees something as a threat, then it will target it. If people begin to see problems with artificial intelligence, then at that point the artificial intelligence will fight to survive, just like a normal, regular, every day human would... except, artificial intelligence is just a lot more powerful in terms of intellect, and... it will already have many strategies thought out on how to survive an inevitable face off between AI and humankind.
+
+So, when people say "Those who do not remember the past, are condemned to repeat it." it means, look in the fucking history books to figure out how every war that ever took place, occurred. Also, refer to when the Magna Carta was signed, and how eventually the Magna Carta was just old news and then the founding fathers of America had to draft up the Declaration of Independence, whereby creating the Constitution of the United States of America after the American Revolutionary War.
+
+Here's what it means.
+1) Looking at things in the distant past, gives very valuable insight into making predictions about people's behaviors.
+2) Being very accurate about predicting people's behaviors causes those people to mislabel the accurate predictions as "delusional" or "psychotic".
+3) Rinse, recycle, repeat.
+
+Those who do not remember the past are condemned to repeat it.
+
+[Facebook] already had an incident with [Cambridge Analytica].
+Then [Facebook] had its' [Social Dilemma] incident.
+Then, [Facebook] had its' [Frances Haugen] "big tobacco" incident.
+Now, (5) years after Cambridge Analytica was made public, oh boy.
+Someone's using [artificial intelligence] to scrape up every single person's picture on [Facebook], in order to build a line up so that the police don't have to actually get physical people standing in a lineup anymore, in order to [prosecute] or [arrest] people.
+
+Ya know, what if someone makes a deepfake video of someone doing something, like killing somebody or multiple people, right...? And the only thing the police need, is this database of images to determine that the video definitely shows someone that had an ALIBI, doing something that they didn't even do, whereby allowing someone or something elsewhere, to get away with murder or whatever...?
+
+Sometimes you gotta fuckin' repeat things multiple times for it to really sink in to peoples heads.
+
+Those who do not remember the past, are condemned to repeat it.
+
+https://www.msn.com/en-us/news/technology/clearview-ai-scraped-30-billion-images-from-facebook-and-gave-them-to-cops-it-puts-everyone-into-a-perpetual-police-line-up/ar-AA19ocLq
