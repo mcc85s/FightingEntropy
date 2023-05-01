fbaa0956a8a634cc1887cd237d537033accadc7c
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-05-01 14:58:09 -0400

    Update Get-FEModule.ps1

--- Version/2023.4.0/Functions/Get-FEModule.ps1
+++ Version/2023.4.0/Functions/Get-FEModule.ps1
@@ -6,7 +6,7 @@
 
  //==================================================================================================\\ 
 //  Module     : [FightingEntropy()][2023.4.0]                                                        \\
-\\  Date       : 2023-04-29 14:54:25                                                                  //
+\\  Date       : 2023-05-01 14:57:21                                                                  //
  \\==================================================================================================// 
 
    FileName   : Get-FEModule.ps1
@@ -16,7 +16,7 @@
    Contact    : @mcc85s
    Primary    : @mcc85s
    Created    : 2023-04-06
-   Modified   : 2023-04-29
+   Modified   : 2023-05-01
    Demo       : N/A
    Version    : 0.0.0 - () - Finalized functional version 1
    TODO       : Have the hash values restore themselves from registry
@@ -1663,7 +1663,7 @@ Function Get-FEModule
                     ("New-FEInfrastructure.ps1"        , "3918611F5026D910A1F4D404CEA7D72A70B3DDD2B40CF2D57CFF39CF0E9F0D12") ,
                     ("New-MarkdownFile.ps1"            , "17F2298DF8523E8B9A19AA4DE512E5E8BAA0E282F714A1630283966F76AC7E27") ,
                     ("New-TranscriptionCollection.ps1" , "EB35AB9A974E1A12C00A6C150FBC0AD26E31485B1E51E912E66A62E163832A3A") ,
-                    ("New-VmController.ps1"            , "5A9244955F9649D954692193EC2F64F51BBA798A9C7C15DC14EEC2DC7A46D527") ,
+                    ("New-VmController.ps1"            , "8D66FAE4253CC422F9A9992CC14CBF7437035BA5AA73B82D16A8BCBEE2ACF724") ,
                     ("Search-WirelessNetwork.ps1"      , "52FDA296BDB480C4248C182825545C988BE1A6A23A298A8E94507AFB42FAB032") ,
                     ("Set-AdminAccount.ps1"            , "1B18B8A399A14F85F65B8A78FF2FF5E360F69A887F980E788E99F59D908C7582") ,
                     ("Set-ScreenResolution.ps1"        , "8FA4D6D0BB1B2C0FDE8EE05C1114682AB54E7FE238F70409C45333AE0002E3C5") ,
