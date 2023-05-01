45f5622aae0a1d0484ecc4bcbf9ccc4dc6624bb6
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-05-01 15:50:16 -0400

    Update Get-FEModule.ps1

--- Version/2023.4.0/Functions/Get-FEModule.ps1
+++ Version/2023.4.0/Functions/Get-FEModule.ps1
@@ -6,7 +6,7 @@
 
  //==================================================================================================\\ 
 //  Module     : [FightingEntropy()][2023.4.0]                                                        \\
-\\  Date       : 2023-05-01 15:43:37                                                                  //
+\\  Date       : 2023-05-01 15:49:46                                                                  //
  \\==================================================================================================// 
 
    FileName   : Get-FEModule.ps1
@@ -1663,7 +1663,7 @@ Function Get-FEModule
                     ("New-FEInfrastructure.ps1"        , "3918611F5026D910A1F4D404CEA7D72A70B3DDD2B40CF2D57CFF39CF0E9F0D12") ,
                     ("New-MarkdownFile.ps1"            , "17F2298DF8523E8B9A19AA4DE512E5E8BAA0E282F714A1630283966F76AC7E27") ,
                     ("New-TranscriptionCollection.ps1" , "EB35AB9A974E1A12C00A6C150FBC0AD26E31485B1E51E912E66A62E163832A3A") ,
-                    ("New-VmController.ps1"            , "32DE2F2187B4275868F92226301884377A4614EF8335588ECD9C6A2750FA6D11") ,
+                    ("New-VmController.ps1"            , "335FC9C48CE3EA6A3A2E216487EEB8C10CCBE5BF0070002E4CE9455726EE0FA3") ,
                     ("Search-WirelessNetwork.ps1"      , "52FDA296BDB480C4248C182825545C988BE1A6A23A298A8E94507AFB42FAB032") ,
                     ("Set-AdminAccount.ps1"            , "1B18B8A399A14F85F65B8A78FF2FF5E360F69A887F980E788E99F59D908C7582") ,
                     ("Set-ScreenResolution.ps1"        , "8FA4D6D0BB1B2C0FDE8EE05C1114682AB54E7FE238F70409C45333AE0002E3C5") ,
