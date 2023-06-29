bfc098c08c536399bc665d07450f949ca77f3661
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-06-29 17:06:21 -0400

    Update Get-FEModule.ps1

--- Version/2023.4.0/Functions/Get-FEModule.ps1
+++ Version/2023.4.0/Functions/Get-FEModule.ps1
@@ -6,7 +6,7 @@
 
  //==================================================================================================\\ 
 //  Module     : [FightingEntropy()][2023.4.0]                                                        \\
-\\  Date       : 2023-06-10 11:59:27                                                                  //
+\\  Date       : 2023-06-29 17:05:55                                                                  //
  \\==================================================================================================// 
 
    FileName   : Get-FEModule.ps1
@@ -16,7 +16,7 @@
    Contact    : @mcc85s
    Primary    : @mcc85s
    Created    : 2023-04-06
-   Modified   : 2023-06-10
+   Modified   : 2023-06-29
    Demo       : N/A
    Version    : 0.0.0 - () - Finalized functional version 1
    TODO       : Have the hash values restore themselves from registry
@@ -1685,7 +1685,7 @@ Function Get-FEModule
                     ("New-FEInfrastructure.ps1"        , "3918611F5026D910A1F4D404CEA7D72A70B3DDD2B40CF2D57CFF39CF0E9F0D12") ,
                     ("New-MarkdownFile.ps1"            , "17F2298DF8523E8B9A19AA4DE512E5E8BAA0E282F714A1630283966F76AC7E27") ,
                     ("New-TranscriptionCollection.ps1" , "D3A5F59E17A71B9D6983F5C4F1F32B88B7634FA4910B2DB4A840D97D2B459C6B") ,
-                    ("New-VmController.ps1"            , "9B19B7DE292A868D0866949BE51BB30D91BD3F49AD5177439DA1B5196CF2A113") ,
+                    ("New-VmController.ps1"            , "19E183DC51FB67F8BF95DEC6A18747F1A9DAF333BA2ADBD1D72E64B13B0CDCCE") ,
                     ("Search-WirelessNetwork.ps1"      , "52FDA296BDB480C4248C182825545C988BE1A6A23A298A8E94507AFB42FAB032") ,
                     ("Set-AdminAccount.ps1"            , "1B18B8A399A14F85F65B8A78FF2FF5E360F69A887F980E788E99F59D908C7582") ,
                     ("Set-ScreenResolution.ps1"        , "8FA4D6D0BB1B2C0FDE8EE05C1114682AB54E7FE238F70409C45333AE0002E3C5") ,
@@ -2444,4 +2444,3 @@ Function Get-FEModule
         }
     }
 }
-
