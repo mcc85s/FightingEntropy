47b6e61d52f49818477bedc4a204a7d8bb5d5ecb
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-08-08 15:43:53 -0400

    Update New-TranscriptionCollection.ps1

--- Version/2023.8.0/Functions/New-TranscriptionCollection.ps1
+++ Version/2023.8.0/Functions/New-TranscriptionCollection.ps1
@@ -5,18 +5,18 @@
 .NOTES
 
  //==================================================================================================\\ 
-//  Module     : [FightingEntropy()][2023.4.0]                                                       \\
-\\  Date       : 2023-04-07 10:53:19                                                                  //
+//  Module     : [FightingEntropy()][2023.8.0]                                                       \\
+\\  Date       : 2023-08-08 15:42:57                                                                  //
  \\==================================================================================================// 
 
     FileName   : New-TranscriptionCollection.ps1
-    Solution   : [FightingEntropy()][2023.4.0]
+    Solution   : [FightingEntropy()][2023.8.0]
     Purpose    : For categorizing transcriptions of (a single/multiple) audio recording(s)
     Author     : Michael C. Cook Sr.
     Contact    : @mcc85s
     Primary    : @mcc85s
     Created    : 2023-04-05
-    Modified   : 2023-04-07
+    Modified   : 2023-08-08
     Demo       : N/A
     Version    : 0.0.0 - () - Finalized functional version 1
     TODO       : N/A
