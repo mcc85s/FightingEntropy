4912f8dc98ccce4c6bab5e6b46b121b1076cdb9c
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-08-08 15:56:00 -0400

    Update Write-Element.ps1

--- Version/2023.8.0/Functions/Write-Element.ps1
+++ Version/2023.8.0/Functions/Write-Element.ps1
@@ -5,18 +5,18 @@
 .NOTES
 
  //==================================================================================================\\ 
-//  Module     : [FightingEntropy()][2023.4.0]                                                        \\
-\\  Date       : 2023-04-05 10:21:30                                                                  //
+//  Module     : [FightingEntropy()][2023.8.0]                                                        \\
+\\  Date       : 2023-08-08 15:55:23                                                                  //
  \\==================================================================================================// 
 
     FileName   : Write-Element.ps1
-    Solution   : [FightingEntropy()][2023.4.0]
+    Solution   : [FightingEntropy()][2023.8.0]
     Purpose    : For creating writing components
     Author     : Michael C. Cook Sr.
     Contact    : @mcc85s 
     Primary    : @mcc85s 
     Created    : 2023-04-05
-    Modified   : 2023-04-05
+    Modified   : 2023-08-08
     Demo       : N/A 
     Version    : 0.0.0 - () - Finalized functional version 1.
     TODO       : Testing various
