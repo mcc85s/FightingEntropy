86010c15d3adef12c31a5f7e082c4ae335d4962a
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-08-08 15:38:26 -0400

    Update New-FEConsole.ps1

--- Version/2023.8.0/Functions/New-FEConsole.ps1
+++ Version/2023.8.0/Functions/New-FEConsole.ps1
@@ -5,18 +5,18 @@
 .NOTES
 
  //==================================================================================================\\ 
-//  Module     : [FightingEntropy()][2023.4.0]                                                        \\
-\\  Date       : 2023-05-16 11:39:37                                                                  //
+//  Module     : [FightingEntropy()][2023.8.0]                                                        \\
+\\  Date       : 2023-08-08 15:37:28                                                                  //
  \\==================================================================================================// 
 
     FileName   : New-FEConsole.ps1
-    Solution   : [FightingEntropy()][2023.4.0]
+    Solution   : [FightingEntropy()][2023.8.0]
     Purpose    : Meant to collect status/time information for a console or etc.
     Author     : Michael C. Cook Sr.
     Contact    : @mcc85s
     Primary    : @mcc85s
     Created    : 2023-04-05
-    Modified   : 2023-05-16
+    Modified   : 2023-08-08
     Demo       : N/A
     Version    : 0.0.0 - () - Finalized functional version 1
     TODO       : -
