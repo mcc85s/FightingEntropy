14baa794e4ebc7ebe66522e8219a2c15b53ca311
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-08-08 15:55:16 -0400

    Update Update-PowerShell.ps1

--- Version/2023.8.0/Functions/Update-PowerShell.ps1
+++ Version/2023.8.0/Functions/Update-PowerShell.ps1
@@ -5,18 +5,18 @@
 .NOTES
 
  //==================================================================================================\\ 
-//  Module     : [FightingEntropy()][2023.4.0]                                                        \\
-\\  Date       : 2023-04-05 10:21:08                                                                  //
+//  Module     : [FightingEntropy()][2023.8.0]                                                        \\
+\\  Date       : 2023-08-08 15:54:52                                                                  //
  \\==================================================================================================// 
 
     FileName   : Update-PowerShell.ps1
-    Solution   : [FightingEntropy()][2023.4.0]
+    Solution   : [FightingEntropy()][2023.8.0]
     Purpose    : Updates PowerShell
     Author     : Michael C. Cook Sr.
     Contact    : @mcc85s
     Primary    : @mcc85s
     Created    : 2023-04-05
-    Modified   : 2023-04-05
+    Modified   : 2023-08-08
     Demo       : N/A
     Version    : 0.0.0 - () - Finalized functional version 1
     TODO       : N/A
