76df95bdf86c67efd907b34a3fd8f90830947416
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-08-08 15:52:08 -0400

    Update Show-ToastNotification.ps1

--- Version/2023.8.0/Functions/Show-ToastNotification.ps1
+++ Version/2023.8.0/Functions/Show-ToastNotification.ps1
@@ -5,18 +5,18 @@
 .NOTES
 
  //==================================================================================================\\ 
-//  Module     : [FightingEntropy()][2023.4.0]                                                        \\
-\\  Date       : 2023-04-05 10:19:57                                                                  //
+//  Module     : [FightingEntropy()][2023.8.0]                                                        \\
+\\  Date       : 2023-08-08 15:51:30                                                                  //
  \\==================================================================================================// 
 
     FileName   : Show-ToastNotification.ps1
-    Solution   : [FightingEntropy()][2023.4.0]
+    Solution   : [FightingEntropy()][2023.8.0]
     Purpose    : Almost like Burnt Toast (which is probably cooler than this is)
     Author     : Michael C. Cook Sr.
     Contact    : @mcc85s
     Primary    : @mcc85s
     Created    : 2023-04-05
-    Modified   : 2023-04-05
+    Modified   : 2023-08-08
     Demo       : N/A
     Version    : 0.0.0 - () - Finalized functional version 1
     TODO       : N/A
