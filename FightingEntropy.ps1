c3896b4c722aea2e0dce503fc894013d0800ab81
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-08-09 15:11:55 -0400

    Update FightingEntropy.ps1

--- FightingEntropy.ps1
+++ FightingEntropy.ps1
@@ -1,1 +1,1 @@
-irm https://github.com/mcc85s/FightingEntropy/blob/main/Version/2023.4.0/FightingEntropy.ps1?raw=true
+irm https://github.com/mcc85s/FightingEntropy/blob/main/Version/2023.8.0/FightingEntropy.ps1?raw=true
