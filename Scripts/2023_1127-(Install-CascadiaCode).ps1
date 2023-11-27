9652361eb7c77694a53d137200f3744d5364e46c
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-11-27 16:42:19 -0500

    Create 2023_1127-(Install-CascadiaCode).ps1

---
+++ Scripts/2023_1127-(Install-CascadiaCode).ps1
@@ -0,0 +1,27 @@
+# Install Cascadia Code
+$Source      = "https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip"
+$Name        = (Split-Path $Source -Leaf).Replace(".zip","")
+$Base        = "$Home\Downloads"
+$Destination = "$Base\$Name.zip"
+$Extract     = "$Base\$Name"
+
+Start-BitsTransfer -Source $Source -Destination $Destination -Verbose
+
+If (Test-Path $Destination)
+{
+    Expand-Archive -Path $Destination -DestinationPath $Extract -Verbose
+}
+
+$List        = Get-ChildItem -Path $Extract -Recurse | ? Extension -match "(ttf|ttc|otf)" | ? Name -match "^CascadiaCode\-"
+
+If ($List.Count -gt 1)
+{
+    $Target      = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    ForEach ($Item in $List)
+    {
+        $Target.CopyHere($Item.Fullname,0x10)
+    }
+}
+
+Remove-Item $Destination -Verbose
+Remove-Item $Extract\* -Recurse -Confirm:0 -Verbose
