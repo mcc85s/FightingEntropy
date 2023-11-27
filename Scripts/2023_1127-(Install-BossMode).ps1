884c7dedbe5e0612fa4c6cb5fcf527fcc299abf9
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2023-11-27 17:04:05 -0500

    Create 2023_1127-(Install-BossMode).ps1

---
+++ Scripts/2023_1127-(Install-BossMode).ps1
@@ -0,0 +1,15 @@
+# Must make changes to Install-BossMode to the extensions.json file for Visual Studio Code
+    {
+        "identifier":
+        {
+            "id":"undefined_publisher.bossmode"
+        },
+        "version":"0.0.1",
+        "location":
+        {
+            "$mid":1,
+            "path":"/c:/Users/mcook85/.vscode/extensions/bossmode",
+            "scheme":"file"
+        },
+        "relativeLocation":"bossmode"
+    }
