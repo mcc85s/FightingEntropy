446173fae4c105aa38c0eaa53c127cbf9374d714
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2024-01-13 01:01:36 -0500

    Update Get-MdtModule.ps1

--- Version/2024.1.0/Functions/Get-MdtModule.ps1
+++ Version/2024.1.0/Functions/Get-MdtModule.ps1
@@ -6,7 +6,7 @@
 
  //==================================================================================================\\ 
 //  Module     : [FightingEntropy()][2024.1.0]                                                        \\
-\\  Date       : 2024-01-11 19:00:25                                                                  //
+\\  Date       : 2024-01-12 21:35:11                                                                  //
  \\==================================================================================================// 
 
     FileName   : Get-MdtModule.ps1
@@ -18,7 +18,7 @@
     Contact    : @mcc85s
     Primary    : @mcc85s
     Created    : 2023-04-05
-    Modified   : 2024-01-11
+    Modified   : 2024-01-12
     Demo       : N/A
     Version    : 0.0.0 - () - Finalized functional version 1
     TODO       : N/A
@@ -29,6 +29,100 @@ Function Get-MdtModule
 {
     [CmdLetBinding(DefaultParameterSetName=0)]
     Param([Parameter(ParameterSetName=1)][Switch]$Install)
+
+    Class WindowsPackageExtension
+    {
+        [UInt32]           $Index
+        [String]            $Name
+        [String]         $Version
+        WindowsPackageExtension([UInt32]$Index,[Object]$Package)
+        {
+            $This.Index    = $Index
+            $This.Name     = $Package.Name
+            $This.Version  = $Package.Version
+        }
+        [String] ToString()
+        {
+            Return "<FEModule.Windows.Package.Extension>"
+        }
+    }
+
+    Class WindowsPackageList
+    {
+        [Object] $Output
+        WindowsPackageList()
+        {
+            $This.Refresh()
+        }
+        [Object] WindowsPackageExtension([UInt32]$Index,[Object]$Package)
+        {
+            Return [WindowsPackageExtension]::New($Index,$Package)
+        }
+        [Object[]] GetPackage()
+        {
+            Return Get-Package | Sort-Object Name
+        }
+        Clear()
+        {
+            $This.Output = @( )
+        }
+        Refresh()
+        {
+            $This.Clear()
+
+            $Package = $This.GetPackage()
+
+            ForEach ($Item in $Package)
+            {
+                $This.Output += $This.WindowsPackageExtension($This.Output.Count,$Item)
+            }
+        }
+        [String] ToString()
+        {
+            Return "<FEModule.Windows.Package.List>"
+        }
+    }
+
+    Class MdtByteSize
+    {
+        [String]   $Name
+        [UInt64]  $Bytes
+        [String]   $Unit
+        [String]   $Size
+        MdtByteSize([String]$Name,[UInt64]$Bytes)
+        {
+            $This.Name   = $Name
+            $This.Bytes  = $Bytes        
+            $This.GetUnit() 
+            $This.GetSize()
+        }
+        GetUnit()
+        {
+            $This.Unit   = Switch ($This.Bytes)
+            {
+                {$_ -lt 1KB}                 {     "Byte" }
+                {$_ -ge 1KB -and $_ -lt 1MB} { "Kilobyte" }
+                {$_ -ge 1MB -and $_ -lt 1GB} { "Megabyte" }
+                {$_ -ge 1GB -and $_ -lt 1TB} { "Gigabyte" }
+                {$_ -ge 1TB}                 { "Terabyte" }
+            }
+        }
+        GetSize()
+        {
+            $This.Size   = Switch -Regex ($This.Unit)
+            {
+                ^Byte     {     "{0} B" -f  $This.Bytes      }
+                ^Kilobyte { "{0:n2} KB" -f ($This.Bytes/1KB) }
+                ^Megabyte { "{0:n2} MB" -f ($This.Bytes/1MB) }
+                ^Gigabyte { "{0:n2} GB" -f ($This.Bytes/1GB) }
+                ^Terabyte { "{0:n2} TB" -f ($This.Bytes/1TB) }
+            }
+        }
+        [String] ToString()
+        {
+            Return $This.Size
+        }
+    }
 
     Enum MdtDependencyType
     {
@@ -43,6 +137,7 @@ Function Get-MdtModule
         [String]             $Name
         [String]      $DisplayName
         [Version]         $Version
+        [Object]             $Size
         [UInt32]        $Installed
         Hidden [String]  $Resource
         Hidden [String]      $Path
@@ -57,14 +152,19 @@ Function Get-MdtModule
         {
             $This.DisplayName = $List[0]
             $This.Version     = $List[1]
-            $This.Resource    = $List[2]
-            $This.Path        = $List[3]
-            $This.File        = $List[4]
-            $This.Arguments   = $List[5]
+            $This.Size        = $This.MdtByteSize($List[2])
+            $This.Resource    = $List[3]
+            $This.Path        = $List[4]
+            $This.File        = $List[5]
+            $This.Arguments   = $List[6]
         }
         [String] FilePath()
         {
             Return "{0}\{1}" -f $This.Path, $This.File
+        }
+        [Object] MdtByteSize([UInt64]$Bytes)
+        {
+            Return [MdtByteSize]::New($This.Name,$Bytes)
         }
         [String] ToString()
         {
@@ -92,9 +192,21 @@ Function Get-MdtModule
         {
             Return [MdtDependencyItem]::New($Name)
         }
+        [Object] WindowsPackageList()
+        {
+            Return [WindowsPackageList]::New()
+        }
+        [String[]] WinPEPackages()
+        {
+            Return "Windows Assessment and Deployment Kit Windows Preinstallation Environment Add-ons - Windows 10",
+            "Windows PE ARM ARM64",
+            "Windows PE ARM ARM64 wims",  
+            "Windows PE x86 x64",         
+            "Windows PE x86 x64 wims"
+        }
         [UInt32] Arch()
         {
-            Return @{x86 = 86; AMD64 = 64 }[$Env:Processor_Architecture]
+            Return @{x86 = 86; AMD64 = 64}[$Env:Processor_Architecture]
         }
         [String[]] RegistryString()
         {
@@ -139,6 +251,7 @@ Function Get-MdtModule
                     {
                         "Microsoft Deployment Toolkit",
                         "6.3.8450.0000",
+                        "80530636.8"
                         "https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x$Arch.msi",
                         "$Env:ProgramData\Tools\Mdt",
                         "MicrosoftDeploymentToolkit_x$Arch.msi",
@@ -146,8 +259,9 @@ Function Get-MdtModule
                     }
                     WinAdk
                     {
-                        "Windows Assessment and Deployment Kit",
+                        "Windows Assessment and Deployment Kit - Windows 10",
                         "10.1.17763.1",
+                        "1256277934.08",
                         "https://go.microsoft.com/fwlink/?linkid=2086042",
                         "$Env:ProgramData\Tools\WinAdk",
                         "winadk1903.exe",
@@ -157,6 +271,7 @@ Function Get-MdtModule
                     {
                         "Windows Preinstallation Environment",
                         "10.1.17763.1",
+                        "6163278069.76",
                         "https://go.microsoft.com/fwlink/?linkid=2087112",
                         "$Env:ProgramData\Tools\WinPe",
                         "winpe1903.exe",
@@ -180,22 +295,40 @@ Function Get-MdtModule
 
             $This.Status = [UInt32](0 -notin $This.Output.Installed)
 
-            If ($This.Status)
+            Switch ($This.Status)
             {
-                $This.Path = $This.ToolkitPath()
+                0
+                {
+                    $This.Path = $Null
+                }
+                1
+                {
+                    $This.Path = $This.ToolkitPath()
+                }
             }
+        }
+        [UInt32] CheckInternet()
+        {
+            Return !!(Test-Connection -Count 1 -ComputerName 1.1.1.1)
         }
         Install()
         {
             $This.Refresh()
+            $Complete = $Null
 
             If (!$This.Status)
             {
                 $List = $This.Output | ? Installed -eq 0
 
+                If (!$This.CheckInternet())
+                {
+                    Throw "Internet connection required to install"
+                }
+
                 If ($List.Count -gt 0)
                 {
                     [Net.ServicePointManager]::SecurityProtocol = 3072
+                    $Package = $This.WindowsPackageList()
 
                     ForEach ($Item in $List)
                     {
@@ -210,7 +343,6 @@ Function Get-MdtModule
                         }
     
                         $Process = Start-Process -FilePath $Item.FilePath() -ArgumentList $Item.Arguments -PassThru
-                        
                         Do
                         {
                             $C = 0
@@ -229,11 +361,20 @@ Function Get-MdtModule
                                     {
                                         $C = 0
                                         $This.Refresh()
+                                        If ($Item.Index -ne 2)
+                                        {
+                                            $Complete = $Process.HasExited -or $Item.Installed
+                                        }
+                                        If ($Item.Index -eq 2)
+                                        {
+                                            $Package.Refresh()
+                                            $Complete = $This.WinPEPackages() -in $Package.Output.Name
+                                        }
                                     }
                                 }
                             }
                         }
-                        Until ($Process.HasExited -and $Item.Installed)
+                        Until ($Complete)
                     }
                 }
             }
